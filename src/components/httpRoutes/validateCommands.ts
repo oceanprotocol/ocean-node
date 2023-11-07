73d95b862f4a72dbd5ea8074c1b5b5ba81f6ea1c
paulo-ocean <paulo@oceanprotocol.com>
2023-11-07 15:40:26 +0000

    add commands, expose http and p2p

--- src/components/httpRoutes/validateCommands.ts
+++ src/components/httpRoutes/validateCommands.ts
@@ -1,3 +1,4 @@
+import { isAddress } from 'ethers'
 import { SUPPORTED_PROTOCOL_COMMANDS, PROTOCOL_COMMANDS } from '../../utils/constants.js'
 
 export type ValidateParams = {
@@ -34,6 +35,21 @@ export function validateCommandAPIParameters(requestBody: any): ValidateParams {
       // echo
     } else if (command === PROTOCOL_COMMANDS.ECHO) {
       // nothing special with this one
+      return {
+        valid: true
+      }
+      // nonce
+    } else if (command === PROTOCOL_COMMANDS.NONCE) {
+      // needs a valid and mandatory address
+      if (!requestBody.address || !isAddress(requestBody.address)) {
+        return {
+          valid: false,
+          reason: !requestBody.address
+            ? 'Missing required parameter: "address"'
+            : 'Parameter : "address" is not a valid web3 address',
+          status: 400
+        }
+      }
       return {
         valid: true
       }
