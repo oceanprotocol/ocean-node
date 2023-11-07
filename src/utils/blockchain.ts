c5094aca7ea15897d87cc95259f91682ae5511fc
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2023-11-07 16:15:31 +0200

    updates on provider keys

--- src/utils/blockchain.ts
+++ src/utils/blockchain.ts
@@ -8,19 +8,13 @@ export class Blockchain {
   private supportedChains: string[] = []
 
   public constructor(networks: RPCS, nodeKeys: OceanNodeKeys) {
-    console.log(
-      'process.env.PRIVATE_KEY.substring(2) ',
-      process.env.PRIVATE_KEY.substring(2)
-    )
-    console.log(' OceanNodeKeys private key ', nodeKeys.publicKey.getPrivateKeyBytes())
     const chainIds = Object.keys(networks)
     this.supportedChains = chainIds
     chainIds.forEach((chain) => {
       this.providers[parseInt(chain)] = new ethers.JsonRpcProvider(networks[chain])
     })
 
-    this.signer = new ethers.Wallet(nodeKeys.publicKey.getPrivateKeyBytes())
-    // this.signer = new ethers.Wallet(process.env.PRIVATE_KEY.substring(2))
+    this.signer = new ethers.Wallet(process.env.PRIVATE_KEY.substring(2))
   }
 
   public getSigner(): ethers.Signer {
@@ -34,8 +28,4 @@ export class Blockchain {
   public getSupportedChains(): string[] {
     return this.supportedChains
   }
-
-  //   public getMiddleware(): ethers.providers.Provider {
-  //     return this.middleware
-  //   }
 }
