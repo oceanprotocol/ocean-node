9677109d5509feb3a2554f2f28bc8b9dd3ac1cf6
an7e <an7e@outlook.com>
2023-11-27 20:59:08 +0300

    fix review

--- src/components/core/encryptHandler.ts
+++ src/components/core/encryptHandler.ts
@@ -2,7 +2,9 @@ import { EncryptCommand } from '../../utils/constants'
 import { P2PCommandResponse } from '../../@types'
 import { Readable } from 'stream'
 import * as base58 from 'base58-js'
+import { encrypt, PrivateKey, PublicKey } from 'eciesjs'
 import crypto from 'crypto'
+import { getConfig } from '../../utils/config.js'
 
 export async function handleEncryptCommand(
   task: EncryptCommand
@@ -17,18 +19,25 @@ export async function handleEncryptCommand(
     }
 
     let encryptedData: Buffer
+    const config = await getConfig()
+    const { privateKey } = config.keys
+    const { publicKey } = config.keys
     if (task.encryptionType === 'AES') {
-      const algorithm = 'aes-256-cbc'
-      const initVector = crypto.randomBytes(16)
-      const securityKey = crypto.randomBytes(32)
-      const cipher = crypto.createCipheriv(algorithm, securityKey, initVector)
+      const initVector = publicKey.subarray(0, 16)
+      const cipher = crypto.createCipheriv('aes-256-cbc', privateKey, initVector)
       encryptedData = Buffer.concat([cipher.update(blobData), cipher.final()])
     }
+    if (task.encryptionType === 'ECIES') {
+      const sk = new PrivateKey(privateKey)
+      encryptedData = encrypt(sk.publicKey.toHex(), blobData)
+    }
+
     return {
       stream: Readable.from(encryptedData.toString('hex')),
       status: { httpStatus: 200 }
     }
   } catch (error) {
+    console.log(error)
     return {
       stream: null,
       status: { httpStatus: 500, error: 'Unknown error: ' + error.message }
