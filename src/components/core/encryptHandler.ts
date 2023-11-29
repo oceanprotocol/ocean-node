6f2c03b24052b8155a4b9d75ee2e50dc0625f003
an7e <an7e@outlook.com>
2023-11-29 17:07:03 +0300

    fix review

--- src/components/core/encryptHandler.ts
+++ src/components/core/encryptHandler.ts
@@ -8,13 +8,17 @@ export async function handleEncryptCommand(
   task: EncryptCommand
 ): Promise<P2PCommandResponse> {
   try {
+    // prepare an empty array in case if
     let blobData: Uint8Array = new Uint8Array()
     if (task.encoding === 'string') {
+      // get bytes from basic blob
       blobData = Uint8Array.from(Buffer.from(task.blob))
     }
     if (task.encoding === 'base58') {
+      // get bytes from a blob that is encoded in standard base58
       blobData = base58.base58_to_binary(task.blob)
     }
+    // do encrypt magic
     const encryptedData = await encrypt(blobData, task.encryptionType)
     return {
       stream: Readable.from(encryptedData.toString()),
