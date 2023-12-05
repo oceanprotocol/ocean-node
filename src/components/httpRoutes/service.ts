30ef6ffb57e1f8f99218432c5d70bbe646d9b504
an7e <an7e@outlook.com>
2023-12-05 15:56:34 +0300

    add api

--- src/components/httpRoutes/service.ts
+++ src/components/httpRoutes/service.ts
@@ -5,6 +5,38 @@ import {Readable} from "stream";
 
 export const serviceRoutes = express.Router()
 
+serviceRoutes.get('/decrypt', async (req, res) => {
+    try {
+        res.status(400).send()
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+serviceRoutes.get('/encrypt', async (req, res) => {
+    try {
+        res.status(400).send()
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+serviceRoutes.get('/download', async (req, res) => {
+    try {
+        res.status(400).send()
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+serviceRoutes.get('/initialize', async (req, res) => {
+    try {
+        res.status(400).send()
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+})
+
 serviceRoutes.get('/nonce', async (req, res) => {
     try {
         const userAddress: string = String(req.query.userAddress)
