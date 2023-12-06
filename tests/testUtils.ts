0ab95e596465ba728ffc19b1aa9e060e0d8d46e7
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-12-06 15:26:17 +0300

    Using wallet rather than SIgner + removing web3

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,22 +1,20 @@
-import { Database } from '../src/components/database/index.js'
-import { Signer } from 'ethers'
-import Web3 from 'web3'
+import { Signature, Wallet } from 'ethers'
 
 export async function signMessage(
   message: string,
-  address: string
+  wallet: Wallet
 ): Promise<{ v: string; r: string; s: string }> {
   try {
-    const web3 = new Web3('http://127.0.0.1:8545')
-    let signedMessage = (await web3.eth.sign(message, address)) as string
-    signedMessage = signedMessage.slice(2) // remove 0x
-    const r = '0x' + signedMessage.slice(0, 64)
-    const s = '0x' + signedMessage.slice(64, 128)
-    const v = '0x' + signedMessage.slice(128, 130)
+    const signedMessage = await wallet.signMessage(message)
+    const signature = Signature.from(signedMessage)
 
-    return { v, r, s }
+    return {
+      v: signature.v.toString(),
+      r: signature.r,
+      s: signature.s
+    }
   } catch (e) {
-    console.log('signMessage error', e)
+    console.error('signMessage error', e)
     throw new Error('Signing message failed')
   }
 }
