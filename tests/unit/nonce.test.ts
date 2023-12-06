d880cdf41ff5d23e22d620b4545d51c198f5b889
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-12-06 17:48:52 +0200

    updated test.

--- tests/unit/nonce.test.ts
+++ tests/unit/nonce.test.ts
@@ -45,18 +45,18 @@ describe('handle nonce', () => {
     const url = 'http://localhost:8108/?apiKey=xyz'
     typesense = new Typesense(convertTypesenseConfig(url))
 
-    const existingCollections = await typesense.collections().retrieve()
-    console.log('existingCollections: ', existingCollections)
-    // check existing ones
-    if (existingCollections && existingCollections.length > 0) {
-      let existsNonceCollection = true
-      try {
-        await typesense.collections(nonceSchema.name).retrieve()
-      } catch (error) {
-        existsNonceCollection = false
-        // collection nonce not exists'
-      }
-    }
+    // const existingCollections = await typesense.collections().retrieve()
+    // console.log('existingCollections: ', existingCollections)
+    // // check existing ones
+    // if (existingCollections && existingCollections.length > 0) {
+    //   let existsNonceCollection = true
+    //   try {
+    //     await typesense.collections(nonceSchema.name).retrieve()
+    //   } catch (error) {
+    //     existsNonceCollection = false
+    //     // collection nonce not exists'
+    //   }
+    // }
 
     //   if (existsNonceCollection) {
     //     // check if the document exists
@@ -74,6 +74,11 @@ describe('handle nonce', () => {
   })
 
   it('should validate signature', async () => {
+    await typesense.collections().create(nonceSchema)
+    typesense.collections('nonce').documents().create({
+      id: '0x4cc9DBfc4bEeA8c986c61DAABB350C2eC55e29d1',
+      nonce: 1
+    })
     const wallet = new ethers.Wallet(
       '0xbee525d70c715bee6ca15ea5113e544d13cc1bb2817e07113d0af7755ddb6391'
     )
