b2e15ab7640a13a0d6f71ddaf9484d7d8aa3355f
an7e <an7e@outlook.com>
2023-12-07 18:27:14 +0300

    add api

--- src/components/httpRoutes/aquarius.ts
+++ src/components/httpRoutes/aquarius.ts
@@ -3,6 +3,7 @@ import {streamToString} from "../../utils/util.js";
 import {Readable} from "stream";
 import {handleGetDdoCommand} from "../core/ddoHandler.js";
 import {PROTOCOL_COMMANDS} from "../../utils/constants.js";
+import {handleQueryCommand} from "../core/queryHandler";
 
 export const aquariusRoutes = express.Router()
 
@@ -12,7 +13,40 @@ aquariusRoutes.get('/assets/ddo/:did', async (req, res) => {
         const node = req.oceanNode.getP2PNode()
         const result = await handleGetDdoCommand(node, { id: did, command: PROTOCOL_COMMANDS.GET_DDO })
         if (result.stream) {
-            res.json(await streamToString(result.stream as Readable))
+            const ddo = JSON.parse(await streamToString(result.stream as Readable))
+            res.json(ddo)
+        } else {
+            res.status(result.status.httpStatus).send(result.status.error)
+        }
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+aquariusRoutes.get('/assets/metadata/:did', async (req, res) => {
+    try {
+        const did = req.params.did;
+        const node = req.oceanNode.getP2PNode()
+        const result = await handleGetDdoCommand(node, { id: did, command: PROTOCOL_COMMANDS.GET_DDO })
+        if (result.stream) {
+            const ddo = JSON.parse(await streamToString(result.stream as Readable))
+            res.json(ddo.metadata)
+        } else {
+            res.status(result.status.httpStatus).send(result.status.error)
+        }
+    } catch (error) {
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+aquariusRoutes.post('/assets/metadata/query', async (req, res) => {
+    try {
+        const query = req.body;
+        const node = req.oceanNode.getP2PNode()
+        const result = await handleQueryCommand(node, { query, command: PROTOCOL_COMMANDS.QUERY })
+        if (result.stream) {
+            const queryResult = JSON.parse(await streamToString(result.stream as Readable))
+            res.json(queryResult)
         } else {
             res.status(result.status.httpStatus).send(result.status.error)
         }
