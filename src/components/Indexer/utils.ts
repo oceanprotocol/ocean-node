e09c39ee79c88b54949f45826ef24f93581d2498
Maria Carmina <50501033+mariacarmina@users.noreply.github.com>
2023-12-07 14:52:15 +0200

    Handled MetadataState event. (#135)
    * Added MetadataState event to event processor.
    
    * Added test.
    
    * More tests.
    
    * Debug test.
    
    * tweak.
    
    * more debug.
    
    * more debugging.
    
    * Remove debug logs.
    
    * Added debug log for nft state.
    
    * More debug.
    
    * more debug...
    
    * Refactored if-else block.
    
    * Added more debug logs.
    
    * fix hashes.
    
    * Updated logs. Updated test.
    
    * fix logs.
    
    * fix review.
    
    * Updated review changes.
    
    * Fix condition.
    
    * Added more expect statements.
    
    * Added test for not indexed asset.
    
    * updated test.
    
    * more logs.
    
    * Removed test.

--- src/components/Indexer/utils.ts
+++ src/components/Indexer/utils.ts
@@ -13,7 +13,10 @@ import {
   defaultConsoleTransport,
   getCustomLoggerForModule
 } from '../../utils/logging/Logger.js'
-import { processMetadataCreatedEvent } from './eventProcessor.js'
+import {
+  processMetadataCreatedEvent,
+  processMetadataStateEvent
+} from './eventProcessor.js'
 
 export const INDEXER_LOGGER: CustomNodeLogger = getCustomLoggerForModule(
   LOGGER_MODULE_NAMES.INDEXER,
@@ -133,9 +136,24 @@ const processMetadataEvents = async (
   provider: JsonRpcApiProvider,
   chainId: number
 ): Promise<any> => {
+  INDEXER_LOGGER.logMessage(`event as ${eventType}`, true)
   if (eventType === EVENTS.METADATA_CREATED) {
     try {
       return await processMetadataCreatedEvent(log, chainId, provider)
+    } catch (e) {
+      INDEXER_LOGGER.log(LOG_LEVELS_STR.LEVEl_ERROR, `Error proccessing metadata: ${e}`)
+    }
+  } else if (eventType === EVENTS.METADATA_STATE) {
+    try {
+      INDEXER_LOGGER.log(
+        LOG_LEVELS_STR.LEVEL_INFO,
+        `Found MetadataState event as ${eventType}`
+      )
+      INDEXER_LOGGER.log(
+        LOG_LEVELS_STR.LEVEL_INFO,
+        `Is eventType ${eventType} equal to ${EVENTS.METADATA_STATE}?`
+      )
+      return await processMetadataStateEvent(log, chainId, provider)
     } catch (e) {
       INDEXER_LOGGER.log(LOG_LEVELS_STR.LEVEl_ERROR, `Error proccessing metadata: ${e}`)
     }
