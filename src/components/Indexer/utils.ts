4107f1b49183a4052b241ba621ea4b660a164e8f
paulo-ocean <paulo@oceanprotocol.com>
2023-12-11 14:25:46 +0000

    fix missed merge conflict

--- src/components/Indexer/utils.ts
+++ src/components/Indexer/utils.ts
@@ -121,39 +121,6 @@ export const processChunkLogs = async (
   return {}
 }
 
-<<<<<<< HEAD
-const processMetadataEvents = async (
-  log: ethers.Log,
-  eventType: string,
-  provider: JsonRpcApiProvider,
-  chainId: number
-): Promise<any> => {
-  INDEXER_LOGGER.logMessage(`event as ${eventType}`, true)
-  if (eventType === EVENTS.METADATA_CREATED) {
-    try {
-      return await processMetadataCreatedEvent(log, chainId, provider)
-    } catch (e) {
-      INDEXER_LOGGER.log(LOG_LEVELS_STR.LEVEL_ERROR, `Error proccessing metadata: ${e}`)
-    }
-  } else if (eventType === EVENTS.METADATA_STATE) {
-    try {
-      INDEXER_LOGGER.log(
-        LOG_LEVELS_STR.LEVEL_INFO,
-        `Found MetadataState event as ${eventType}`
-      )
-      INDEXER_LOGGER.log(
-        LOG_LEVELS_STR.LEVEL_INFO,
-        `Is eventType ${eventType} equal to ${EVENTS.METADATA_STATE}?`
-      )
-      return await processMetadataStateEvent(log, chainId, provider)
-    } catch (e) {
-      INDEXER_LOGGER.log(LOG_LEVELS_STR.LEVEL_ERROR, `Error proccessing metadata: ${e}`)
-    }
-  }
-}
-
-=======
->>>>>>> develop
 const procesExchangeCreated = async (): Promise<string> => {
   return 'EXCHANGE_CREATED'
 }
