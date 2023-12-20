d7791dd9f62f08273e2e4fb30c7bc92a018f9a95
Maria Carmina <mariacarmina.cretu@gmail.com>
2023-12-20 11:35:17 +0200

    fix import.

--- src/components/httpRoutes/provider.ts
+++ src/components/httpRoutes/provider.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getNonce } from '../core/handlers/nonceHandler.js'
+import { getNonce } from '../core/handlers/handler.js'
 import { streamToString } from '../../utils/util.js'
 import { Readable } from 'stream'
 import { calculateFee } from '../core/handlers/feesHandler.js'
