024b452eabb3e0940401e7b16abd38932e0ddec4
an7e <an7e@outlook.com>
2023-12-20 14:02:15 +0300

    fix review

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,23 +1,32 @@
-import { Credentials } from '../@types/DDO/Credentials'
+import { Credential, Credentials } from '../@types/DDO/Credentials'
 
+export function findCredential(
+  credentials: Credential[],
+  consumerCredentials: Credential
+) {
+  return credentials.find((credential) => {
+    const credentialType = String(credential?.type).toLowerCase()
+    const credentialValues = credential.values.map((v) => String(v).toLowerCase())
+    return (
+      credentialType === consumerCredentials.type &&
+      credentialValues.includes(consumerCredentials.values[0])
+    )
+  })
+}
 /**
  * This method checks credentials
- * @param credentials ddo credentials
+ * @param credentials credentials
  * @param consumerAddress consumer address
  */
 export function checkCredentials(credentials: Credentials, consumerAddress: string) {
   const consumerCredentials = {
     type: 'address',
-    values: [consumerAddress]
+    values: [String(consumerAddress).toLowerCase()]
   }
 
   // check allow access
   if (Array.isArray(credentials.allow) && credentials.allow.length > 0) {
-    const allowCredential = credentials.allow.find(
-      (credential) =>
-        credential.type === consumerCredentials.type &&
-        credential.values.includes(consumerCredentials.values[0].toLowerCase())
-    )
+    const allowCredential = findCredential(credentials.allow, consumerCredentials)
     if (!allowCredential) {
       return false
     }
@@ -25,11 +34,7 @@ export function checkCredentials(credentials: Credentials, consumerAddress: stri
 
   // check deny access
   if (Array.isArray(credentials.deny) && credentials.deny.length > 0) {
-    const denyCredential = credentials.deny.find(
-      (credential) =>
-        credential.type === consumerCredentials.type &&
-        credential.values.includes(consumerCredentials.values[0].toLowerCase())
-    )
+    const denyCredential = findCredential(credentials.deny, consumerCredentials)
     if (denyCredential) {
       return false
     }
