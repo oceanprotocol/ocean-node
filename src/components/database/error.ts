c47515e1b312ea042da6954d7887e64de598b9d7
Maria Carmina <mariacarmina.cretu@gmail.com>
2024-01-08 18:18:34 +0200

    Added status

--- src/components/database/error.ts
+++ src/components/database/error.ts
@@ -4,6 +4,7 @@ export class DatabaseError extends Error {
 
   constructor(message: string, status?: number) {
     super(message)
+    this.status = status
     this.name = new.target.name
     Object.setPrototypeOf(this, new.target.prototype)
   }
