1466402a47f48a2d42327080829db19fb19298e8
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-01-23 15:42:14 +0300

    Updating util exports

--- src/components/core/queryHandler.ts
+++ src/components/core/queryHandler.ts
@@ -1,17 +1,31 @@
 import { Handler } from './handler.js'
-import { QueryCommand } from '../../utils/constants.js'
+import { QueryCommand, getSchemaVersions } from '../../utils/index.js'
 import { P2PCommandResponse } from '../../@types/OceanNode.js'
 import { Readable } from 'stream'
+import { DDO } from '../../@types/DDO/DDO.js'
 
 export class QueryHandler extends Handler {
   async handle(task: QueryCommand): Promise<P2PCommandResponse> {
     try {
-      let result = await this.getP2PNode().getDatabase().ddo.search(task.query)
-      if (!result) {
-        result = []
+      let aggregatedResults: DDO[] = []
+      const versions = await getSchemaVersions()
+
+      for (const version of versions) {
+        // Modify this line to query the specific collection based on version
+        const result = await this.getP2PNode()
+          .getDatabase()
+          .ddo.searchByVersion(version, task.query)
+        if (result && result.length) {
+          aggregatedResults = aggregatedResults.concat(result)
+        }
       }
+
+      if (aggregatedResults.length === 0) {
+        aggregatedResults = []
+      }
+
       return {
-        stream: Readable.from(JSON.stringify(result)),
+        stream: Readable.from(JSON.stringify(aggregatedResults)),
         status: { httpStatus: 200 }
       }
     } catch (error) {
