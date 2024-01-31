de51d5c07ae288236754e9cf6766b38b66e158a5
paulo-ocean <paulo@oceanprotocol.com>
2024-01-31 12:41:19 +0000

    fix test, singleton might be created already, does not make much sense anyway

--- src/test/unit/networking.test.ts
+++ src/test/unit/networking.test.ts
@@ -1,8 +1,4 @@
-import { OceanNode } from '../../OceanNode.js'
-import { OceanP2P } from '../../components/P2P/index.js'
-import { getDatabase } from '../../utils/database.js'
 import { getConfiguration } from '../../utils/index.js'
-
 import { expect } from 'chai'
 
 describe('Test available network interfaces', async () => {
@@ -11,13 +7,9 @@ describe('Test available network interfaces', async () => {
     const envSet = process.env.INTERFACES
     const config = await getConfiguration()
     const { hasP2P, hasHttp } = config
-    const database = await getDatabase()
     if (!envSet) {
       expect(hasP2P).to.be.equal(true)
       expect(hasHttp).to.be.equal(true)
-      expect(
-        OceanNode.getInstance(database, new OceanP2P(config, database)).getP2PNode()
-      ).to.be.instanceOf(OceanP2P)
     } else {
       try {
         let interfaces = JSON.parse(process.env.INTERFACES) as string[]
