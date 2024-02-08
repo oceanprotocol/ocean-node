47c9b15ca4260134bc7852ef29bc5046797b0109
Maria Carmina <mariacarmina.cretu@gmail.com>
2024-02-08 16:03:14 +0200

    fix type

--- src/@types/Fees.ts
+++ src/@types/Fees.ts
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js'
+
 export type FeeTokens = {
   chain: string // chain id => 137
   token: string // token => token address 0x967da4048cD07aB37855c090aAF366e4ce1b9F48
@@ -16,7 +18,7 @@ export type FeeStrategy = {
 export interface ProviderFeeData {
   providerFeeAddress: string
   providerFeeToken: string
-  providerFeeAmount: number | BigInt
+  providerFeeAmount: number | BigInt | BigNumber
   providerData: any
   v: any
   r: any
