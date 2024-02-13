21084f95ee980712b62238c3be85ce9a6dc92130
an7e <an7e@outlook.com>
2024-02-13 15:50:57 +0300

    fix review

--- src/components/c2d/index.ts
+++ src/components/c2d/index.ts
@@ -1,145 +1,144 @@
-import {OceanNode} from '../../OceanNode.js'
-import {CORE_LOGGER} from '../../utils/logging/common.js'
-import {createHash} from 'crypto'
-import {FindDdoHandler} from '../core/ddoHandler.js'
-import {decrypt} from '../../utils/crypt.js'
-import {Storage} from '../storage/index.js'
-import {getConfiguration} from '../../utils/config.js'
-import {GetEnvironmentsHandler} from '../core/compute.js'
-import {PROTOCOL_COMMANDS} from '../../utils/constants.js'
-import {streamToObject} from '../../utils/util.js'
-import {Readable} from 'stream'
-import {ConsumerParameter} from '../../@types/DDO/ConsumerParameter.js'
-import ERC721Factory from "@oceanprotocol/contracts/artifacts/contracts/ERC721Factory.sol/ERC721Factory.json";
-import ERC721Template
-    from "@oceanprotocol/contracts/artifacts/contracts/templates/ERC721Template.sol/ERC721Template.json";
+import { OceanNode } from '../../OceanNode.js'
+import { CORE_LOGGER } from '../../utils/logging/common.js'
+import { createHash } from 'crypto'
+import { FindDdoHandler } from '../core/ddoHandler.js'
+import { decrypt } from '../../utils/crypt.js'
+import { Storage } from '../storage/index.js'
+import { getConfiguration } from '../../utils/config.js'
+import { GetEnvironmentsHandler } from '../core/compute.js'
+import { PROTOCOL_COMMANDS } from '../../utils/constants.js'
+import { streamToObject } from '../../utils/util.js'
+import { Readable } from 'stream'
+import { ConsumerParameter } from '../../@types/DDO/ConsumerParameter.js'
+import ERC721Factory from '@oceanprotocol/contracts/artifacts/contracts/ERC721Factory.sol/ERC721Factory.json'
+import ERC721Template from '@oceanprotocol/contracts/artifacts/contracts/templates/ERC721Template.sol/ERC721Template.json'
 
 export async function checkEnvironmentExists(envId: string, oceanNode: OceanNode) {
-    const config = await getConfiguration()
-    const {supportedNetworks} = config
-    for (const supportedNetwork of Object.keys(supportedNetworks)) {
-        const getEnvironmentsTask = {
-            command: PROTOCOL_COMMANDS.GET_COMPUTE_ENVIRONMENTS,
-            chainId: parseInt(supportedNetwork)
-        }
-        const response = await new GetEnvironmentsHandler(oceanNode).handle(
-            getEnvironmentsTask
-        )
-        if (response.status.httpStatus === 200) {
-            const computeEnvironments = await streamToObject(response.stream as Readable)
-            for (const computeEnvironment of computeEnvironments) {
-                if (computeEnvironment.id === envId) {
-                    return true
-                }
-            }
-        }
+  const config = await getConfiguration()
+  const { supportedNetworks } = config
+  for (const supportedNetwork of Object.keys(supportedNetworks)) {
+    const getEnvironmentsTask = {
+      command: PROTOCOL_COMMANDS.GET_COMPUTE_ENVIRONMENTS,
+      chainId: parseInt(supportedNetwork)
     }
-    return false
+    const response = await new GetEnvironmentsHandler(oceanNode).handle(
+      getEnvironmentsTask
+    )
+    if (response.status.httpStatus === 200) {
+      const computeEnvironments = await streamToObject(response.stream as Readable)
+      for (const computeEnvironment of computeEnvironments) {
+        if (computeEnvironment.id === envId) {
+          return true
+        }
+      }
+    }
+  }
+  return false
 }
 
 export async function getAlgoChecksums(
-    algoDID: string,
-    algoServiceId: string,
-    oceanNode: OceanNode
+  algoDID: string,
+  algoServiceId: string,
+  oceanNode: OceanNode
 ) {
-    const checksums = {
-        files: '',
-        container: ''
+  const checksums = {
+    files: '',
+    container: ''
+  }
+  try {
+    const algoDDO = await new FindDdoHandler(oceanNode).findAndFormatDdo(algoDID)
+    if (!algoDDO) {
+      throw new Error('Algorithm DDO not found')
     }
-    try {
-        const algoDDO = await new FindDdoHandler(oceanNode).findAndFormatDdo(algoDID)
-        if (!algoDDO) {
-            throw new Error('Algorithm DDO not found')
-        }
-        const algorithmService = algoDDO.services.find(
-            (service) => service.id === algoServiceId
-        )
-        if (!algorithmService) {
-            throw new Error('Algorithm service not found')
-        }
-        const decryptedUrlBytes = await decrypt(
-            Uint8Array.from(Buffer.from(algorithmService.files, 'hex')),
-            'ECIES'
-        )
-        const decryptedFilesString = Buffer.from(decryptedUrlBytes).toString()
-        const decryptedFileArray = JSON.parse(decryptedFilesString)
+    const algorithmService = algoDDO.services.find(
+      (service) => service.id === algoServiceId
+    )
+    if (!algorithmService) {
+      throw new Error('Algorithm service not found')
+    }
+    const decryptedUrlBytes = await decrypt(
+      Uint8Array.from(Buffer.from(algorithmService.files, 'hex')),
+      'ECIES'
+    )
+    const decryptedFilesString = Buffer.from(decryptedUrlBytes).toString()
+    const decryptedFileArray = JSON.parse(decryptedFilesString)
 
-        for (const file of decryptedFileArray.files) {
-            const storage = Storage.getStorageClass(file)
-            const fileInfo = await storage.getFileInfo({type: file.type}, true)
-            checksums.files = checksums.files.concat(fileInfo[0].contentChecksum)
-        }
-        checksums.container = createHash('sha256')
-            .update(
-                algoDDO.metadata.algorithm.container.entrypoint +
-                algoDDO.metadata.algorithm.container.checksum
-            )
-            .digest('hex')
-        return checksums
-    } catch (error) {
-        CORE_LOGGER.error(error.message)
-        return checksums
+    for (const file of decryptedFileArray.files) {
+      const storage = Storage.getStorageClass(file)
+      const fileInfo = await storage.getFileInfo({ type: file.type }, true)
+      checksums.files = checksums.files.concat(fileInfo[0].contentChecksum)
     }
+    checksums.container = createHash('sha256')
+      .update(
+        algoDDO.metadata.algorithm.container.entrypoint +
+          algoDDO.metadata.algorithm.container.checksum
+      )
+      .digest('hex')
+    return checksums
+  } catch (error) {
+    CORE_LOGGER.error(error.message)
+    return checksums
+  }
 }
 
 export async function validateAlgoForDataset(
-    algoDID: string,
-    algoChecksums: {
-        files: string
-        container: string
-    },
-    datasetDID: string,
-    datasetServiceId: string,
-    oceanNode: OceanNode
+  algoDID: string,
+  algoChecksums: {
+    files: string
+    container: string
+  },
+  datasetDID: string,
+  datasetServiceId: string,
+  oceanNode: OceanNode
 ) {
-    try {
-        const datasetDDO = await new FindDdoHandler(oceanNode).findAndFormatDdo(datasetDID)
-        if (!datasetDDO) {
-            throw new Error('Dataset DDO not found')
-        }
-        const datasetService = datasetDDO.services.find(
-            (service) => service.id === datasetServiceId
-        )
-        if (!datasetService) {
-            throw new Error('Dataset service not found')
-        }
-        const {compute} = datasetService
-        if (datasetService.type !== 'compute' || !compute) {
-            throw new Error('Service not compute')
-        }
-
-        if (algoDID) {
-            if (
-                compute.publisherTrustedAlgorithms === undefined &&
-                compute.publisherTrustedAlgorithmPublishers === undefined
-            ) {
-                return true
-            }
-            if (compute.publisherTrustedAlgorithms) {
-                const trustedAlgo = compute.publisherTrustedAlgorithms.find(
-                    (algo) => algo.did === algoDID
-                )
-                if (trustedAlgo) {
-                    return (
-                        trustedAlgo.filesChecksum === algoChecksums.files &&
-                        trustedAlgo.containerSectionChecksum === algoChecksums.container
-                    )
-                }
-                return false
-            }
-            if (compute.publisherTrustedAlgorithmPublishers) {
-                const algoDDO = await new FindDdoHandler(oceanNode).findAndFormatDdo(algoDID)
-                if (algoDDO) {
-                    return compute.publisherTrustedAlgorithmPublishers.includes(algoDDO.nftAddress)
-                }
-                return false
-            }
-            return true
-        }
-
-        return compute.allowRawAlgorithm
-    } catch (error) {
-        CORE_LOGGER.error(error.message)
-        return false
+  try {
+    const datasetDDO = await new FindDdoHandler(oceanNode).findAndFormatDdo(datasetDID)
+    if (!datasetDDO) {
+      throw new Error('Dataset DDO not found')
     }
+    const datasetService = datasetDDO.services.find(
+      (service) => service.id === datasetServiceId
+    )
+    if (!datasetService) {
+      throw new Error('Dataset service not found')
+    }
+    const { compute } = datasetService
+    if (datasetService.type !== 'compute' || !compute) {
+      throw new Error('Service not compute')
+    }
+
+    if (algoDID) {
+      if (
+        compute.publisherTrustedAlgorithms === undefined &&
+        compute.publisherTrustedAlgorithmPublishers === undefined
+      ) {
+        return true
+      }
+      if (compute.publisherTrustedAlgorithms) {
+        const trustedAlgo = compute.publisherTrustedAlgorithms.find(
+          (algo) => algo.did === algoDID
+        )
+        if (trustedAlgo) {
+          return (
+            trustedAlgo.filesChecksum === algoChecksums.files &&
+            trustedAlgo.containerSectionChecksum === algoChecksums.container
+          )
+        }
+        return false
+      }
+      if (compute.publisherTrustedAlgorithmPublishers) {
+        const algoDDO = await new FindDdoHandler(oceanNode).findAndFormatDdo(algoDID)
+        if (algoDDO) {
+          return compute.publisherTrustedAlgorithmPublishers.includes(algoDDO.nftAddress)
+        }
+        return false
+      }
+      return true
+    }
+
+    return compute.allowRawAlgorithm
+  } catch (error) {
+    CORE_LOGGER.error(error.message)
+    return false
+  }
 }
--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,80 +1,81 @@
-import {ConsumerParameter} from "../@types/DDO/ConsumerParameter.js";
-import {CORE_LOGGER} from "./logging/common.js";
+import { ConsumerParameter } from '../@types/DDO/ConsumerParameter.js'
+import { CORE_LOGGER } from './logging/common.js'
 
 function checkString(value: any) {
-    return typeof value === 'string' || value instanceof String
+  return typeof value === 'string' || value instanceof String
 }
 
 function checkBoolean(value: any) {
-    return typeof value === 'boolean' || value instanceof Boolean
+  return typeof value === 'boolean' || value instanceof Boolean
 }
 
 function checkNumber(value: any) {
-    return typeof value === 'number' || value instanceof Number
+  return typeof value === 'number' || value instanceof Number
 }
 
 function checkObject(value: any) {
-    return (
-        typeof value === 'object' &&
-        value !== null &&
-        !Array.isArray(value)
-    );
+  return typeof value === 'object' && value !== null && !Array.isArray(value)
 }
 
-export function validateConsumerParameters(ddoConsumerParameters: ConsumerParameter[], userSentObject: any[]) {
-    const validation = {
-        valid: true,
-        message: ''
+export function validateConsumerParameters(
+  ddoConsumerParameters: ConsumerParameter[],
+  userSentObject: any[]
+) {
+  const validation = {
+    valid: true,
+    message: ''
+  }
+
+  try {
+    if (!Array.isArray(userSentObject)) {
+      throw new Error(`Value is not an array`)
     }
 
-    try {
-        if (!Array.isArray(userSentObject)) {
-            throw new Error(`Value is not an array`)
+    for (const consumerParameter of ddoConsumerParameters) {
+      for (const sentObject of userSentObject) {
+        if (!checkObject(sentObject)) {
+          throw new Error(`Value is not an object`)
         }
-
-        for (const consumerParameter of ddoConsumerParameters) {
-            for (const sentObject of userSentObject) {
-                if (!checkObject(sentObject)) {
-                    throw new Error(`Value is not an object`)
-                }
-                const sentObjectKey = consumerParameter.name
-                if (sentObject[sentObjectKey] == undefined) {
-                    if (consumerParameter.required) {
-                        throw new Error(`value of key ${sentObjectKey} parameter is required`)
-                    }
-                    sentObject[sentObjectKey] = consumerParameter.default
-                }
-                const sentObjectValue = sentObject[sentObjectKey]
-                switch (consumerParameter.type) {
-                    case 'text':
-                        if (!checkString(sentObjectValue)) {
-                            throw new Error(`value ${sentObjectValue} of key ${sentObjectKey} parameter is not a text`)
-                        }
-                        break;
-                    case 'number':
-                        if (!checkNumber(sentObjectValue)) {
-                            throw new Error(`value ${sentObjectValue} of key ${sentObjectKey} parameter is not a number`)
-                        }
-                        break;
-                    case 'boolean':
-                        if (!checkBoolean(sentObjectValue)) {
-                            throw new Error(`value ${sentObjectValue} of key ${sentObjectKey} parameter is not a boolean`)
-                        }
-                        break;
-                    case 'select':
-                        const options: any[] = consumerParameter.options.map(option => Object.keys(option)[0])
-                        if (!options.includes(sentObjectValue)) {
-                            throw new Error(`value ${sentObjectValue} of key ${sentObjectKey} parameter is not a select`)
-                        }
-                        break;
-                }
-            }
+        const sentObjectKey = consumerParameter.name
+        if (sentObject[sentObjectKey] === undefined) {
+          if (consumerParameter.required) {
+            throw new Error(`value of key ${sentObjectKey} parameter is required`)
+          }
+          sentObject[sentObjectKey] = consumerParameter.default
         }
-        return validation
-    } catch (error) {
-        CORE_LOGGER.error(error.message)
-        validation.valid = false
-        validation.message = error.message
-        return validation
+        const sentObjectValue = sentObject[sentObjectKey]
+        if (consumerParameter.type === 'text' && !checkString(sentObjectValue)) {
+          throw new Error(
+            `value ${sentObjectValue} of key ${sentObjectKey} parameter is not a text`
+          )
+        }
+        if (consumerParameter.type === 'number' && !checkString(sentObjectValue)) {
+          throw new Error(
+            `value ${sentObjectValue} of key ${sentObjectKey} parameter is not a number`
+          )
+        }
+        if (consumerParameter.type === 'boolean' && !checkString(sentObjectValue)) {
+          throw new Error(
+            `value ${sentObjectValue} of key ${sentObjectKey} parameter is not a boolean`
+          )
+        }
+        if (consumerParameter.type === 'select') {
+          const options: any[] = consumerParameter.options.map(
+            (option) => Object.keys(option)[0]
+          )
+          if (!options.includes(sentObjectValue)) {
+            throw new Error(
+              `value ${sentObjectValue} of key ${sentObjectKey} parameter is not a select`
+            )
+          }
+        }
+      }
     }
+    return validation
+  } catch (error) {
+    CORE_LOGGER.error(error.message)
+    validation.valid = false
+    validation.message = error.message
+    return validation
+  }
 }
