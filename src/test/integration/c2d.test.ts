5cf915046bf3cb58f55dab3fccf8212c20632d1b
an7e <an7e@outlook.com>
2024-02-16 14:51:15 +0300

    fix test

--- src/test/integration/c2d.test.ts
+++ src/test/integration/c2d.test.ts
@@ -192,7 +192,8 @@ describe('C2D functions', async () => {
     assert(txReceipt, 'set metadata failed')
   })
 
-  it('should publish DatasetDDO', async () => {
+  it('should publish DatasetDDO', async function () {
+    this.timeout(65000) // Extend default Mocha test timeout
     const tx = await (factoryContract as any).createNftWithErc20(
       {
         name: '72120Bundle',
