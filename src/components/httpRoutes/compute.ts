1b230da890aecb8e4a9573daef80536f2df8c658
alexcos20 <alex.coseru@gmail.com>
2024-02-26 11:23:34 +0200

    add all http routes

--- src/components/httpRoutes/compute.ts
+++ src/components/httpRoutes/compute.ts
@@ -63,7 +63,7 @@ computeRoutes.get('/api/services/computeEnvironments', async (req, res) => {
 computeRoutes.post('/api/services/compute', async (req, res) => {
   try {
     HTTP_LOGGER.logMessage(
-      `GET computeEnvironments request received with query: ${JSON.stringify(req.query)}`,
+      `ComputeStartCommand request received with query: ${JSON.stringify(req.query)}`,
       true
     )
 
@@ -88,6 +88,82 @@ computeRoutes.post('/api/services/compute', async (req, res) => {
     const response = await new ComputeStartHandler().handle(startComputeTask) // get compute environments
     const jobs = await streamToObject(response.stream as Readable)
     res.status(200).json(jobs)
+  } catch (error) {
+    HTTP_LOGGER.log(LOG_LEVELS_STR.LEVEL_ERROR, `Error: ${error}`)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+computeRoutes.put('/api/services/compute', async (req, res) => {
+  try {
+    HTTP_LOGGER.logMessage(
+      `ComputeStopCommand request received with query: ${JSON.stringify(req.query)}`,
+      true
+    )
+
+    const stopComputeTask: ComputeStopCommand = {
+      command: PROTOCOL_COMMANDS.COMPUTE_STOP,
+      node: (req.query.node as string) || null,
+      consumerAddress: (req.query.consumerAddress as string) || null,
+      signature: (req.query.signature as string) || null,
+      nonce: (req.query.nonce as string) || null,
+      jobId: (req.query.jobId as string) || null
+    }
+    const response = await new ComputeStopHandler().handle(stopComputeTask)
+    const jobs = await streamToObject(response.stream as Readable)
+    res.status(200).json(jobs)
+  } catch (error) {
+    HTTP_LOGGER.log(LOG_LEVELS_STR.LEVEL_ERROR, `Error: ${error}`)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+computeRoutes.get('/api/services/compute', async (req, res) => {
+  try {
+    HTTP_LOGGER.logMessage(
+      `ComputeGetStatusCommand request received with query: ${JSON.stringify(req.query)}`,
+      true
+    )
+    const statusComputeTask: ComputeGetStatusCommand = {
+      command: PROTOCOL_COMMANDS.COMPUTE_GET_STATUS,
+      node: (req.query.node as string) || null,
+      consumerAddress: (req.query.consumerAddress as string) || null,
+      did: (req.query.did as string) || null,
+      jobId: (req.query.jobId as string) || null
+    }
+    const response = await new ComputeGetStatusHandler().handle(statusComputeTask)
+    const jobs = await streamToObject(response.stream as Readable)
+    res.status(200).json(jobs)
+  } catch (error) {
+    HTTP_LOGGER.log(LOG_LEVELS_STR.LEVEL_ERROR, `Error: ${error}`)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+computeRoutes.get('/api/services/computeResult', async (req, res) => {
+  try {
+    HTTP_LOGGER.logMessage(
+      `ComputeGetResultCommand request received with query: ${JSON.stringify(req.query)}`,
+      true
+    )
+    const resultComputeTask: ComputeGetResultCommand = {
+      command: PROTOCOL_COMMANDS.COMPUTE_GET_STATUS,
+      node: (req.query.node as string) || null,
+      consumerAddress: (req.query.consumerAddress as string) || null,
+      index: parseInt(req.query.index as string) || null,
+      jobId: (req.query.jobId as string) || null,
+      signature: (req.query.signature as string) || null,
+      nonce: (req.query.nonce as string) || null
+    }
+
+    const response = await new ComputeGetResultHandler().handle(resultComputeTask)
+    if (response.stream) {
+      res.status(response.status.httpStatus)
+      res.set(response.status.headers)
+      response.stream.pipe(res)
+    } else {
+      res.status(response.status.httpStatus).send(response.status.error)
+    }
   } catch (error) {
     HTTP_LOGGER.log(LOG_LEVELS_STR.LEVEL_ERROR, `Error: ${error}`)
     res.status(500).send('Internal Server Error')
