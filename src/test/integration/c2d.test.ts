973c87bbc3b87d517aed60e896e074c0699a12ef
paulo-ocean <paulo@oceanprotocol.com>
2024-03-05 15:44:45 +0000

    try fix lint and tests

--- src/test/integration/c2d.test.ts
+++ src/test/integration/c2d.test.ts
@@ -22,21 +22,23 @@ import ERC721Template from '@oceanprotocol/contracts/artifacts/contracts/templat
 import { publishAlgoDDO, publishDatasetDDO } from '../data/ddo.js'
 import { createHash } from 'crypto'
 import { encrypt } from '../../utils/crypt.js'
-import { delay, waitToIndex } from './testUtils.js'
+import { delay, expectedTimeoutFailure, waitToIndex } from './testUtils.js'
 import { Database } from '../../components/database/index.js'
 import { OceanIndexer } from '../../components/Indexer/index.js'
 import {
   ENVIRONMENT_VARIABLES,
+  EVENTS,
   getConfiguration,
   PROTOCOL_COMMANDS
 } from '../../utils/index.js'
 import { OceanNodeConfig } from '../../@types/OceanNode.js'
 import { Readable } from 'stream'
-import { UrlFileObject } from '../../@types/fileObject.js'
+import { EncryptMethod, FileObjectType, UrlFileObject } from '../../@types/fileObject.js'
 import { FileInfoHandler } from '../../components/core/fileInfoHandler.js'
 import { OceanNode } from '../../OceanNode.js'
 import { ConsumerParameter } from '../../@types/DDO/ConsumerParameter'
 import {
+  DEFAULT_TEST_TIMEOUT,
   OverrideEnvConfig,
   buildEnvOverrideConfig,
   getMockSupportedNetworks,
@@ -44,10 +46,11 @@ import {
   tearDownEnvironment
 } from '../utils/utils.js'
 
-describe('C2D functions', async () => {
+describe('C2D functions', () => {
   let config: OceanNodeConfig
   let database: Database
   let oceanNode: OceanNode
+  // eslint-disable-next-line no-unused-vars
   let indexer: OceanIndexer
   let provider: JsonRpcProvider
   let publisherAccount: Signer
@@ -104,7 +107,7 @@ describe('C2D functions', async () => {
     const fileInfoTask = {
       command: PROTOCOL_COMMANDS.FILE_INFO,
       file: urlFile,
-      type: 'url' as 'url'
+      type: FileObjectType.URL
     }
     const response = await new FileInfoHandler(oceanNode).handle(fileInfoTask)
 
@@ -174,7 +177,7 @@ describe('C2D functions', async () => {
       ]
     }
     const filesData = Uint8Array.from(Buffer.from(JSON.stringify(files)))
-    algoDDO.services[0].files = await encrypt(filesData, 'ECIES')
+    algoDDO.services[0].files = await encrypt(filesData, EncryptMethod.ECIES)
 
     const metadata = hexlify(Buffer.from(JSON.stringify(algoDDO)))
     const hash = createHash('sha256').update(metadata).digest('hex')
@@ -244,7 +247,7 @@ describe('C2D functions', async () => {
       ]
     }
     const filesData = Uint8Array.from(Buffer.from(JSON.stringify(files)))
-    datasetDDO.services[0].files = await encrypt(filesData, 'ECIES')
+    datasetDDO.services[0].files = await encrypt(filesData, EncryptMethod.ECIES)
 
     datasetDDO.services[0].compute = {
       allowRawAlgorithm: false,
@@ -279,37 +282,60 @@ describe('C2D functions', async () => {
 
   delay(50000)
 
-  it('should getAlgoChecksums', async () => {
-    const algoDDOTest = await waitToIndex(algoDDO.id, database)
-    const algoChecksums = await getAlgoChecksums(
-      algoDDOTest.id,
-      algoDDOTest.services[0].id,
-      oceanNode
+  it('should getAlgoChecksums', async function () {
+    const { ddo, wasTimeout } = await waitToIndex(
+      algoDDO.id,
+      EVENTS.METADATA_CREATED,
+      DEFAULT_TEST_TIMEOUT
     )
-    expect(algoChecksums.files).to.equal(
-      'f6a7b95e4a2e3028957f69fdd2dac27bd5103986b2171bc8bfee68b52f874dcd'
-    )
-    expect(algoChecksums.container).to.equal(
-      'ba8885fcc7d366f058d6c3bb0b7bfe191c5f85cb6a4ee3858895342436c23504'
-    )
+    const algoDDOTest = ddo
+    if (algoDDOTest) {
+      const algoChecksums = await getAlgoChecksums(
+        algoDDOTest.id,
+        algoDDOTest.services[0].id,
+        oceanNode
+      )
+      expect(algoChecksums.files).to.equal(
+        'f6a7b95e4a2e3028957f69fdd2dac27bd5103986b2171bc8bfee68b52f874dcd'
+      )
+      expect(algoChecksums.container).to.equal(
+        'ba8885fcc7d366f058d6c3bb0b7bfe191c5f85cb6a4ee3858895342436c23504'
+      )
+    } else expect(expectedTimeoutFailure(this.test.title)).to.be.equal(wasTimeout)
   })
 
-  it('should validateAlgoForDataset', async () => {
-    const algoDDOTest = await waitToIndex(algoDDO.id, database)
-    const algoChecksums = await getAlgoChecksums(
-      algoDDOTest.id,
-      algoDDOTest.services[0].id,
-      oceanNode
+  it('should validateAlgoForDataset', async function () {
+    const { ddo, wasTimeout } = await waitToIndex(
+      algoDDO.id,
+      EVENTS.METADATA_CREATED,
+      DEFAULT_TEST_TIMEOUT
     )
-    const datasetDDOTest = await waitToIndex(datasetDDO.id, database)
-    const result = await validateAlgoForDataset(
-      algoDDOTest.id,
-      algoChecksums,
-      datasetDDOTest.id,
-      datasetDDOTest.services[0].id,
-      oceanNode
-    )
-    expect(result).to.equal(true)
+
+    const algoDDOTest = ddo
+    if (algoDDOTest) {
+      const algoChecksums = await getAlgoChecksums(
+        algoDDOTest.id,
+        algoDDOTest.services[0].id,
+        oceanNode
+      )
+      const { ddo, wasTimeout } = await waitToIndex(
+        datasetDDO.id,
+        EVENTS.METADATA_CREATED,
+        DEFAULT_TEST_TIMEOUT
+      )
+
+      const datasetDDOTest = ddo
+      if (datasetDDOTest) {
+        const result = await validateAlgoForDataset(
+          algoDDOTest.id,
+          algoChecksums,
+          datasetDDOTest.id,
+          datasetDDOTest.services[0].id,
+          oceanNode
+        )
+        expect(result).to.equal(true)
+      } else expect(expectedTimeoutFailure(this.test.title)).to.be.equal(wasTimeout)
+    } else expect(expectedTimeoutFailure(this.test.title)).to.be.equal(wasTimeout)
   })
 
   it('should validateConsumerParameters', async () => {
