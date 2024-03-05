2c4ebbd964859fea0d0de7dc221f87a3858f3fa2
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-03-05 17:16:40 +0300

    Adding script for building the dashboard

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
   output: 'export',
+  distDir: '../dist/dashboard'
 }
 
 module.exports = nextConfig
