40b23026cc73b44940cf7ad2d20623269c5017fd
Maria Carmina <mariacarmina.cretu@gmail.com>
2024-03-08 15:47:08 +0200

    fixed call.

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,19 +1,28 @@
 import { sha256, toUtf8Bytes, verifyMessage } from 'ethers'
 import { HTTP_LOGGER } from './logging/common.js'
-import { existsEnvironmentVariable } from './config.js'
+import { PROTOCOL_COMMANDS } from './constants.js'
+import { StatusHandler } from '../components/core/statusHandler.js'
+import { streamToObject } from './util.js'
+import { Readable } from 'stream'
+import { OceanNode } from '../OceanNode.js'
 
-export function validateSignature(expiryTimestamp: number, signature: string): boolean {
+export async function validateSignature(
+  expiryTimestamp: number,
+  signature: string,
+  oceanNode: OceanNode
+): Promise<boolean> {
   try {
     const message = sha256(toUtf8Bytes(expiryTimestamp.toString()))
 
     const signerAddress = verifyMessage(message, signature).toLowerCase()
     HTTP_LOGGER.logMessage(`Resolved signer address: ${signerAddress}`)
-
-    if (!existsEnvironmentVariable('ALLOWED_ADMINS')) {
-      HTTP_LOGGER.logMessage(`Missing env var for ALLOWED_ADMINS`)
+    const statusCommand = {
+      command: PROTOCOL_COMMANDS.STATUS
     }
+    const response = await new StatusHandler(oceanNode).handle(statusCommand)
+    const status = await streamToObject(response.stream as Readable)
     const currentTimestamp = new Date().getTime()
-    for (const address of JSON.parse(process.env.ALLOWED_ADMINS)) {
+    for (const address of status.allowedAdmins) {
       if (address.lowercase() === signerAddress && currentTimestamp < expiryTimestamp) {
         return true
       }
