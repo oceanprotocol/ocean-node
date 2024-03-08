43e65f46abf5ea7b1e878d57a663d38164262d73
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-03-08 16:39:08 +0300

    adjusting next.js settings for local development

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -4,8 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  output: 'export',
-  distDir: '../dist/dashboard'
+  // Only apply "output: 'export'" when not in development mode
+  ...(process.env.NODE_ENV !== 'development' && { output: 'export' }),
+  ...(process.env.NODE_ENV !== 'development' && { distDir: '../dist/dashboard' })
 }
 
 module.exports = nextConfig
