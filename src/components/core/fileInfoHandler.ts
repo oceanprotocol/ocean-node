5cba13c98ee15a3ea4262375c60ff2e7b431c93d
Maria Carmina <mariacarmina.cretu@gmail.com>
2024-03-19 18:20:23 +0200

    add logs in file infor handler.

--- src/components/core/fileInfoHandler.ts
+++ src/components/core/fileInfoHandler.ts
@@ -128,6 +128,7 @@ export class FileInfoHandler extends Handler {
         })
       } else if (task.did && task.serviceId) {
         const fileArray = await getFile(task.did, task.serviceId, oceanNode)
+        CORE_LOGGER.logMessage(`fileArray: ${fileArray.toString()}`)
 
         if (task.fileIndex) {
           const fileMetadata = await formatMetadata(fileArray[task.fileIndex])
