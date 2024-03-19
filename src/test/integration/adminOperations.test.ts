61365e30826f38a5c76eca5167a49caa891edd6f
Maria Carmina <mariacarmina.cretu@gmail.com>
2024-03-19 12:36:42 +0200

    more logs + waiting for indexing.

--- src/test/integration/adminOperations.test.ts
+++ src/test/integration/adminOperations.test.ts
@@ -6,9 +6,8 @@ import { OceanNode } from '../../OceanNode.js'
 import { RPCS } from '../../@types/blockchain.js'
 import { downloadAsset } from '../data/assets.js'
 import { publishAsset } from '../utils/assets.js'
-// import { waitToIndex } from './testUtils.js'
 import {
-  //   DEFAULT_TEST_TIMEOUT,
+  DEFAULT_TEST_TIMEOUT,
   OverrideEnvConfig,
   buildEnvOverrideConfig,
   getMockSupportedNetworks,
@@ -17,7 +16,7 @@ import {
 } from '../utils/utils.js'
 
 import {
-  //   EVENTS,
+  EVENTS,
   ENVIRONMENT_VARIABLES,
   PROTOCOL_COMMANDS,
   getConfiguration
@@ -39,6 +38,7 @@ import {
   ReindexTxHandler,
   StopNodeHandler
 } from '../../components/core/adminOperations.js'
+import { waitToIndex } from './testUtils.js'
 
 describe('Should test admin operations', () => {
   let config: OceanNodeConfig
@@ -123,6 +123,12 @@ describe('Should test admin operations', () => {
 
   it('should publish compute datasets & algos', async () => {
     publishedDataset = await publishAsset(downloadAsset, publisherAccount)
+    console.log(`published dataset: ${JSON.stringify(publishAsset)}`)
+    await waitToIndex(
+      publishedDataset.ddo.id,
+      EVENTS.METADATA_CREATED,
+      DEFAULT_TEST_TIMEOUT
+    )
   })
 
   it('should pass for reindex tx command', async () => {
