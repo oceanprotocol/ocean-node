f8022515835a774e426a41cdbd2c7937cb91e829
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-03-21 12:36:03 +0300

    Updated styling

--- dashboard/src/components/Admin/index.tsx
+++ dashboard/src/components/Admin/index.tsx
@@ -3,20 +3,27 @@ import styles from './index.module.css'
 import DownloadLogs from '../Admin/DownloadLogs'
 import StopNode from '../Admin/StopNode'
 import { useAdminContext } from '@/context/AdminProvider'
+import { useAccount } from 'wagmi'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 export default function AdminActions() {
-  const { generateSignature, signature, validTimestamp } = useAdminContext()
+  const { generateSignature, signature, validTimestamp, admin } = useAdminContext()
+  const { isConnected } = useAccount()
 
   return (
     <div className={styles.root}>
       <div className={styles.title}>ADMIN ACTIONS</div>
+      {!isConnected && <ConnectButton />}
+      {isConnected && !admin && (
+        <div className={styles.connecting}>Your account does not have admin access</div>
+      )}
 
-      {(!signature || !validTimestamp) && (
+      {(!signature || !validTimestamp) && admin && (
         <button type="button" className={styles.unlockButton} onClick={generateSignature}>
           Unlock
         </button>
       )}
-      {signature && validTimestamp && (
+      {signature && validTimestamp && admin && (
         <>
           <DownloadLogs />
           <StopNode />
