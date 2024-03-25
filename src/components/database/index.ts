9337f919d5d33e60309083a74b31871831c0b1e6
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-03-25 17:22:50 +0300

    Adding pagination to search method on ddo database

--- src/components/database/index.ts
+++ src/components/database/index.ts
@@ -252,19 +252,43 @@ export class DdoDatabase {
     }
   }
 
-  async search(query: Record<string, any>) {
+  async search(
+    query: Record<string, any>,
+    maxResultsPerPage?: number,
+    pageNumber?: number
+  ) {
     try {
+      let queryObj: TypesenseSearchParams
+      // if queryObj is a string
+      if (typeof query === 'string') {
+        queryObj = JSON.parse(query)
+      } else {
+        queryObj = query as TypesenseSearchParams
+      }
+
+      const maxPerPage = maxResultsPerPage ? Math.min(maxResultsPerPage, 250) : 250 // Cap maxResultsPerPage at 250
+      const page = pageNumber || 1 // Default to the first page if pageNumber is not provided
       const results = []
+
       for (const schema of this.schemas) {
+        // Extend the query with pagination parameters
+        const searchParams: TypesenseSearchParams = {
+          ...queryObj,
+          per_page: maxPerPage,
+          page
+        }
+
         const result = await this.provider
           .collections(schema.name)
           .documents()
-          .search(query as TypesenseSearchParams)
+          .search(searchParams)
         results.push(result)
       }
+
       return results
     } catch (error) {
-      const errorMsg = `Error when searching by query ${query}: ` + error.message
+      const errorMsg =
+        `Error when searching by query ${JSON.stringify(query)}: ` + error.message
       DATABASE_LOGGER.logMessageWithEmoji(
         errorMsg,
         true,
