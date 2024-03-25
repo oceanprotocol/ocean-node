18602053087431dbed8ed84e2d2949fab64347d8
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-03-25 23:56:30 +0300

    Updating logs route to use new optional parameters

--- src/components/httpRoutes/logs.ts
+++ src/components/httpRoutes/logs.ts
@@ -4,30 +4,44 @@ export const logRoutes = express.Router()
 
 logRoutes.get('/logs', async (req, res) => {
   try {
-    // Ensure the query parameters are strings before creating Date objects
     const startTime =
       typeof req.query.startTime === 'string'
         ? new Date(req.query.startTime)
         : new Date(Date.now() - 24 * 60 * 60 * 1000) // Default to 24 hours ago
     const endTime =
-      typeof req.query.endTime === 'string' ? new Date(req.query.endTime) : new Date()
+      typeof req.query.endTime === 'string' ? new Date(req.query.endTime) : new Date() // Default to now
     const maxLogs =
       typeof req.query.maxLogs === 'string' ? parseInt(req.query.maxLogs, 10) : 100 // Default to 100 logs
     const moduleName =
       typeof req.query.moduleName === 'string' ? req.query.moduleName : undefined
     const level = typeof req.query.level === 'string' ? req.query.level : undefined
+    const maxLogsPerPage =
+      typeof req.query.maxLogsPerPage === 'string'
+        ? Math.min(parseInt(req.query.maxLogsPerPage, 10), 250)
+        : 250 // Default to 250, capped at 250
+    const page =
+      typeof req.query.page === 'string' ? parseInt(req.query.page, 10) : undefined // Default to undefined, which will fetch all logs
 
-    // Retrieve logs from the database
+    // Retrieve logs from the database with pagination
     const logs = await req.oceanNode
       .getDatabase()
-      .logs.retrieveMultipleLogs(startTime, endTime, maxLogs, moduleName, level)
+      .logs.retrieveMultipleLogs(
+        startTime,
+        endTime,
+        maxLogs,
+        moduleName,
+        level,
+        maxLogsPerPage,
+        page
+      )
+
     if (logs) {
       res.json(logs)
     } else {
       res.status(404).send('No logs found')
     }
   } catch (error) {
-    res.status(500).send('Internal Server Error')
+    res.status(500).send(`Internal Server Error: ${error.message}`)
   }
 })
 
