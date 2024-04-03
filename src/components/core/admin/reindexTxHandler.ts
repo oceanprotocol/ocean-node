8037f45b3edcab0520ed7ff03bad29ef0712070b
Maria Carmina <mariacarmina.cretu@gmail.com>
2024-04-03 16:45:26 +0300

    removed commented code.

--- src/components/core/admin/reindexTxHandler.ts
+++ src/components/core/admin/reindexTxHandler.ts
@@ -11,7 +11,6 @@ import { P2PCommandResponse } from '../../../@types/OceanNode.js'
 import { CORE_LOGGER } from '../../../utils/logging/common.js'
 import { ReadableString } from '../../P2P/handleProtocolCommands.js'
 import { checkSupportedChainId } from '../../../utils/blockchain.js'
-// import { processChunkLogs } from '../../Indexer/utils.js'
 import { OceanIndexer } from '../../Indexer/index.js'
 
 export class ReindexTxHandler extends AdminHandler {
@@ -37,24 +36,6 @@ export class ReindexTxHandler extends AdminHandler {
       )
     }
     try {
-      // const blockchain = new Blockchain(checkChainId.networkRpc, task.chainId)
-      // const provider = blockchain.getProvider()
-      // const signer = blockchain.getSigner()
-      // const receipt = await provider.getTransactionReceipt(task.txId)
-      // if (!receipt) {
-      //   CORE_LOGGER.error(`Tx receipt was not found for txId ${task.txId}`)
-      //   return buildErrorResponse(`Tx receipt was not found for txId ${task.txId}`)
-      // }
-      // const { logs } = receipt
-      // const ret = await processChunkLogs(logs, signer, provider, task.chainId)
-      // if (!ret) {
-      //   CORE_LOGGER.error(
-      //     `Reindex tx for txId ${task.txId} failed on chain ${task.chainId}.`
-      //   )
-      //   return buildErrorResponse(
-      //     `Reindex tx for txId ${task.txId} failed on chain ${task.chainId}.`
-      //   )
-      // }
       OceanIndexer.addReindexTask({
         txId: task.txId,
         chainId: task.chainId.toString()
