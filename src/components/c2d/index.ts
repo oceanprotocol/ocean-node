9815a31020ec04ed7b5f5552d24bdd9ad80b35af
paulo-ocean <paulo@oceanprotocol.com>
2024-04-11 13:30:16 +0100

    fix import

--- src/components/c2d/index.ts
+++ src/components/c2d/index.ts
@@ -1,7 +1,7 @@
 import { OceanNode } from '../../OceanNode.js'
 import { CORE_LOGGER } from '../../utils/logging/common.js'
 import { createHash } from 'crypto'
-import { FindDdoHandler } from '../core/ddoHandler.js'
+import { FindDdoHandler } from '../core/handler/ddoHandler.js'
 import { decrypt } from '../../utils/crypt.js'
 import { Storage } from '../storage/index.js'
 import { getConfiguration } from '../../utils/config.js'
