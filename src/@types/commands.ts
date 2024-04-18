167b73005beb086fbe5c2c0faaad75548ae3aa36
paulo-ocean <paulo@oceanprotocol.com>
2024-04-18 12:32:31 +0100

    add detail status handler, small refactor

--- src/@types/commands.ts
+++ src/@types/commands.ts
@@ -53,6 +53,7 @@ export interface ValidateDDOCommand extends Command {
 }
 
 export interface StatusCommand extends Command {}
+export interface DetailedStatusCommand extends StatusCommand {}
 export interface EchoCommand extends Command {}
 
 export interface StopNodeCommand extends Command {
