626a355e0cc754925198c0cd2347918cc63abbf2
Maria Carmina <mariacarmina.cretu@gmail.com>
2024-04-23 22:24:08 +0300

    debug

--- src/components/Indexer/processor.ts
+++ src/components/Indexer/processor.ts
@@ -322,7 +322,9 @@ export class MetadataEventProcessor extends BaseEventProcessor {
           JSON.stringify(ddo)
         )}, decodedEventData.args[4]: ${JSON.stringify(
           decodedEventData.args[4]
-        )}, hash 4: ${create256Hash(JSON.stringify(decodedEventData.args[4]))}`
+        )}, hash 4: ${create256Hash(
+          JSON.stringify(decodedEventData.args[4])
+        )}, decoded data: ${JSON.stringify(decodedEventData)}`
       )
 
       // if (
