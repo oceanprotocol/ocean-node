dc9b2550521c5efc62f930087bc4c56365698340
paulo-ocean <paulo@oceanprotocol.com>
2024-05-08 11:42:11 +0100

    more timeout onanother test

--- src/test/integration/transactionValidation.test.ts
+++ src/test/integration/transactionValidation.test.ts
@@ -97,12 +97,13 @@ describe('validateOrderTransaction Function with Orders', () => {
   })
 
   it('should publish a dataset', async function () {
+    this.timeout(DEFAULT_TEST_TIMEOUT * 2)
     publishedDataset = await publishAsset(genericDDO, publisherAccount)
 
     const { ddo, wasTimeout } = await waitToIndex(
       publishedDataset.ddo.id,
       EVENTS.METADATA_CREATED,
-      DEFAULT_TEST_TIMEOUT
+      DEFAULT_TEST_TIMEOUT * 2
     )
 
     if (!ddo) {
