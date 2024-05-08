90e0ca920a88693433013d096a0a0f9fdd4f302e
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-05-08 19:18:03 +0300

    Creating form for defining which chain ID should be reindexed

--- dashboard/src/components/Admin/ReindexChain.tsx
+++ dashboard/src/components/Admin/ReindexChain.tsx
@@ -2,16 +2,19 @@ import React, { useState } from 'react'
 import styles from './index.module.css'
 import { useAdminContext } from '@context/AdminProvider'
 import Button from '@mui/material/Button'
+import { TextField } from '@mui/material'
 
 export default function ReIndexChain() {
+  const [showChainInput, setShowChainInput] = useState(false)
   const [isLoading, setLoading] = useState(false)
+  const [chainId, setChainId] = useState<string>()
   const { signature, expiryTimestamp } = useAdminContext()
 
   async function reIndex() {
     setLoading(true)
     try {
       const apiUrl = '/directCommand'
-      if (expiryTimestamp && signature) {
+      if (expiryTimestamp && signature && chainId) {
         await fetch(apiUrl, {
           headers: {
             Accept: 'application/json',
@@ -20,7 +23,7 @@ export default function ReIndexChain() {
           method: 'POST',
           body: JSON.stringify({
             command: 'reindexChain',
-            chainId: '137',
+            chainId,
             expiryTimestamp,
             signature
           })
@@ -34,13 +37,28 @@ export default function ReIndexChain() {
     }
   }
 
-  const Spinner = () => {
-    return <span className={styles.loader}></span>
-  }
-
   return (
-    <Button onClick={reIndex} variant="outlined" color="error">
-      {isLoading ? <Spinner /> : <div>ReIndex Chain</div>}
-    </Button>
+    <div className={styles.column}>
+      <Button type="button" onClick={() => setShowChainInput(!showChainInput)}>
+        Download Logs
+      </Button>
+
+      {showChainInput && (
+        <div className={styles.filters}>
+          <TextField
+            label="Chain ID"
+            type="number"
+            value={chainId}
+            onChange={(e) => setChainId(e.target.value)}
+            fullWidth
+            margin="normal"
+            variant="outlined"
+          />
+          <Button type="button" onClick={reIndex} variant="outlined" disabled={isLoading}>
+            Download
+          </Button>
+        </div>
+      )}
+    </div>
   )
 }
