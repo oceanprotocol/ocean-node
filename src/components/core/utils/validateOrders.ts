04211458fce9b8066bf54bad5d401689c27061cc
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2024-05-15 15:47:43 +0300

    more logs

--- src/components/core/utils/validateOrders.ts
+++ src/components/core/utils/validateOrders.ts
@@ -56,12 +56,15 @@ export async function validateOrderTransaction(
     }
   }
   const erc20Address = txReceiptMined.to
+  console.log(' erc20Address', erc20Address)
   const datatokenContract = new Contract(
     erc20Address,
     ERC20Template.abi,
     await provider.getSigner()
   )
+
   const erc721Address = await datatokenContract.getERC721Address()
+  console.log(' erc721Address', erc721Address)
 
   const orderReusedEvent = fetchEventFromTransaction(
     txReceiptMined,
