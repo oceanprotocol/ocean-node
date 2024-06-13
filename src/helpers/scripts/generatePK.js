a2a325cc1e6192f30b6d6cee67d1f86385d7e8ab
paulo-ocean <paulo@oceanprotocol.com>
2024-06-13 11:09:10 +0100

    wip: update scripts

--- src/helpers/scripts/generatePK.js
+++ src/helpers/scripts/generatePK.js
@@ -1,14 +1,21 @@
 //simple script to generate a new private key
 import * as ethCrypto from 'eth-crypto'
 import fs from 'node:fs'
+
+const argv = process.argv.slice(2)
+const saveToFile = argv.length === 1 && argv[0] === '--save'
 const pair = ethCrypto.createIdentity()
-console.log('\n#########################################\n')
-console.log('WARNING: SENSITIVE DATA!\n')
-console.log('-------------------------------------------\n')
-console.log('\nYour Node Private Key: \n', pair.privateKey)
-console.log('\nYour Node Public Key:\n', pair.publicKey)
-console.log('\nYour Node Eth/Wallet Address:\n', pair.address)
-console.log('\n#########################################\n')
-const pkFile = fs.createWriteStream('.pk.out', { flags: 'w' })
-pkFile.write(pair.privateKey)
-pkFile.end()
+if (saveToFile) {
+  console.log('Saving private key to file ".pk.out"')
+  const pkFile = fs.createWriteStream('.pk.out', { flags: 'w' })
+  pkFile.write(pair.privateKey)
+  pkFile.end()
+} else {
+  console.log('\n#########################################\n')
+  console.log('\tWARNING: SENSITIVE DATA!\n')
+  console.log('#########################################\n')
+  console.log('\nYour Node Private Key: \n', pair.privateKey)
+  console.log('\nYour Node Public Key:\n', pair.publicKey)
+  console.log('\nYour Node Eth/Wallet Address:\n', pair.address)
+  console.log('\n-------------------------------------------\n')
+}
--- src/helpers/scripts/setupNodeEnv.sh
+++ src/helpers/scripts/setupNodeEnv.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
+#basic check on the length
+check_pk() {
+   pk=$1
+   echo "Entered private key: $pk"
+   length=${#pk}
+   echo "Size is $length"
+   if [ $length -lt 64 ]; then
+     echo "The provided private Key seems invalid!"
+     exit 1
+   fi 
+}
+#check if .env exists
+check_env_file() {
+    if ! [ -f '../../../.env' ]; then
+        exists_env_file=0
+    else
+        exists_env_file=1
+    fi
+}
+#create .env from .env.example
+create_env_file() {
+    if [ -f '../../../.env.example' ]; then
+        `cp ../../../.env.example ../../../.env`
+        created_env_file=1
+    else
+        echo "Could not find the example file, aborting!"
+        exit 1
+    fi
+}
+
 if ! [ -f '.pk.out' ]; then
   echo "Private Key File does not exist."
   read -p "Do you want me to generate one for you? [y/n]: " generate_key_answer
   generate_key_answer=${generate_key_answer:-n}
-  echo "Generating key? $generate_key_answer"
+ 
   if [ "$generate_key_answer" == 'y' ]; then
     # run the script
-    `node ./generatePK.js > /dev/null`
+    `node ./generatePK.js --save > /dev/null`
     # read the file contents
     PRIVATE_KEY=`cat .pk.out`
     echo "Generated Private Key: $PRIVATE_KEY"
   else
         read -p "Enter your private key: " PRIVATE_KEY
         echo "Entered private key: $PRIVATE_KEY"
-        length=${#PRIVATE_KEY}
-        echo "Size is $length"
-        if [ $length -lt 64 ]; then
-            echo "The provided private Key seems invalid!"
-            exit
-        fi
-
+        check_pk $PRIVATE_KEY
    fi
 else
     echo "We found a Private Key File."
@@ -32,24 +56,33 @@ else
     else
         read -p "Enter your private key: " PRIVATE_KEY
         echo "Entered private key: $PRIVATE_KEY"
-        length=${#PRIVATE_KEY}
-        echo "Size is $length"
-        if [ $length -lt 64 ]; then
-            echo "The provided private Key seems invalid!"
-            exit
-        fi
+        check_pk $PRIVATE_KEY
     fi
 fi
 
-read -p "Enter your node HTTP port: " HTTP_API_PORT
-HTTP_API_PORT=${HTTP_API_PORT:-8001}
-export HTTP_API_PORT=$HTTP_API_PORT
-export P2P_ipV4BindTcpPort=8000
-export PRIVATE_KEY=$private_key
-export DB_URL=http://localhost:8108/?apiKey=xyz
-export IPFS_GATEWAY=https://ipfs.io/
-export ARWEAVE_GATEWAY=https://arweave.net/
-export OPERATOR_SERVICE_URL=[\"http://localhost:31000/\"]
-export ALLOWED_ADMINS=[\"0xC3b17E73671502614485337995dc13d54DE2F0Ad\"]
-export RPCS="{ \"8996\": {\"rpc\": \"http://127.0.0.1:8545\", \"chainId\": 8996, \"network\": \"development\", \"chunkSize\": 100}}"
-export RPCS="{ \"11155420\":{ \"rpc\":\"https://sepolia.optimism.io\", \"startBlock\": 11916901, \"chainId\": 11155420, \"network\": \"optimism-sepolia\", \"chunkSize\": 100 }}"
+check_env_file
+#if does not exists, create it from template or ask input
+if [ $exists_env_file -eq 0 ]; then
+    echo "Initial .env file not detected!"
+    read -p "Do you want me to create one from the template? [y/n]: " create_env_file
+    create_env_file=${create_env_file:-y}
+    echo "Creating env file?: $create_env_file"
+    if [ "$create_env_file" == 'y' ]; then
+        create_env_file
+        if [ $created_env_file -eq 1 ]; then
+            echo "------------------------------------------------------------------------------"
+            echo "Successfully created .env file. Please update it before starting the node!"
+            echo "Once you're done with your changes, run 'source .env' (on your ocean node root folder)," 
+            echo "before building/starting the node, in order to apply the environment changes."
+            echo "------------------------------------------------------------------------------"
+        fi
+    else 
+        echo "Creating .env file aborted!"
+        created_env_file=0
+    fi
+fi
+echo "Done!"
+
+
+
+
