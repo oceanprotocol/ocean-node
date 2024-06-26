85c523c35cfe0bdfc42407a9d1f2a376e72b0d75
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-06-26 11:10:56 +0300

    Update

--- dist/dashboard/_next/static/wFLBNMtsMGC18AtXEFWjq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/2477-ff31ed06ca3bb6b0.js","static/css/1ba71e685b597d17.css","static/chunks/pages/index-eca8ffef37dfbef5.js"],"/_error":["static/chunks/pages/_error-e4216aab802f5810.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
