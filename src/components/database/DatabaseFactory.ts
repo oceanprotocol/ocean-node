c8d7bab119a9a33a5445707b2c7f2145d82801d4
AdriGeorge <adrian7ko@gmail.com>
2024-09-12 12:56:52 +0300

    fix: db

--- src/components/database/DatabaseFactory.ts
+++ src/components/database/DatabaseFactory.ts
@@ -34,74 +34,58 @@ import { ElasticSearchMetadataQuery } from './ElasticSearchMetadataQuery.js'
 
 export class DatabaseFactory {
   static createNonceDatabase(config: OceanNodeDBConfig): AbstractNonceDatabase {
-    if (process.env.DB_TYPE === 'typesense') {
-      return new TypesenseNonceDatabase(config, typesenseSchemas.nonceSchemas)
-    } else if (process.env.DB_TYPE === 'elasticsearch') {
+    if (process.env.DB_TYPE === 'elasticsearch') {
       return new ElasticsearchNonceDatabase(config)
     }
-    throw new Error('Unsupported database type')
+    return new TypesenseNonceDatabase(config, typesenseSchemas.nonceSchemas)
   }
 
   static createDdoDatabase(config: OceanNodeDBConfig): AbstractDdoDatabase {
-    if (process.env.DB_TYPE === 'typesense') {
-      return new TypesenseDdoDatabase(config, typesenseSchemas.ddoSchemas)
-    } else if (process.env.DB_TYPE === 'elasticsearch') {
+    if (process.env.DB_TYPE === 'elasticsearch') {
       return new ElasticsearchDdoDatabase(config, elasticSchemas.ddoSchemas)
     }
-    throw new Error('Unsupported database type')
+    return new TypesenseDdoDatabase(config, typesenseSchemas.ddoSchemas)
   }
 
   static createIndexerDatabase(config: OceanNodeDBConfig): AbstractIndexerDatabase {
-    if (process.env.DB_TYPE === 'typesense') {
-      return new TypesenseIndexerDatabase(config, typesenseSchemas.indexerSchemas)
-    } else if (process.env.DB_TYPE === 'elasticsearch') {
+    if (process.env.DB_TYPE === 'elasticsearch') {
       return new ElasticsearchIndexerDatabase(config)
     }
-    throw new Error('Unsupported database type')
+    return new TypesenseIndexerDatabase(config, typesenseSchemas.indexerSchemas)
   }
 
   static createLogDatabase(config: OceanNodeDBConfig): AbstractLogDatabase {
-    if (process.env.DB_TYPE === 'typesense') {
-      return new TypesenseLogDatabase(config, typesenseSchemas.logSchemas)
-    } else if (process.env.DB_TYPE === 'elasticsearch') {
+    if (process.env.DB_TYPE === 'elasticsearch') {
       return new ElasticsearchLogDatabase(config)
     }
-    throw new Error('Unsupported database type')
+    return new TypesenseLogDatabase(config, typesenseSchemas.logSchemas)
   }
 
   static createOrderDatabase(config: OceanNodeDBConfig): AbstractOrderDatabase {
-    if (process.env.DB_TYPE === 'typesense') {
-      return new TypesenseOrderDatabase(config, typesenseSchemas.orderSchema)
-    } else if (process.env.DB_TYPE === 'elasticsearch') {
+    if (process.env.DB_TYPE === 'elasticsearch') {
       return new ElasticsearchOrderDatabase(config, elasticSchemas.orderSchema)
     }
-    throw new Error('Unsupported database type')
+    return new TypesenseOrderDatabase(config, typesenseSchemas.orderSchema)
   }
 
   static createDdoStateDatabase(config: OceanNodeDBConfig): AbstractDdoStateDatabase {
-    if (process.env.DB_TYPE === 'typesense') {
-      return new TypesenseDdoStateDatabase(config, typesenseSchemas.ddoStateSchema)
-    } else if (process.env.DB_TYPE === 'elasticsearch') {
+    if (process.env.DB_TYPE === 'elasticsearch') {
       return new ElasticsearchDdoStateDatabase(config)
     }
-    throw new Error('Unsupported database type')
+    return new TypesenseDdoStateDatabase(config, typesenseSchemas.ddoStateSchema)
   }
 
   static createDdoStateQuery(): IDdoStateQuery {
-    if (process.env.DB_TYPE === 'typesense') {
-      return new TypesenseDdoStateQuery()
-    } else if (process.env.DB_TYPE === 'elasticsearch') {
+    if (process.env.DB_TYPE === 'elasticsearch') {
       return new ElasticSearchDdoStateQuery()
     }
-    throw new Error('Unsupported database type')
+    return new TypesenseDdoStateQuery()
   }
 
   static createMetadataQuery(): IMetadataQuery {
-    if (process.env.DB_TYPE === 'typesense') {
-      return new TypesenseMetadataQuery()
-    } else if (process.env.DB_TYPE === 'elasticsearch') {
+    if (process.env.DB_TYPE === 'elasticsearch') {
       return new ElasticSearchMetadataQuery()
     }
-    throw new Error('Unsupported database type')
+    return new TypesenseMetadataQuery()
   }
 }
--- src/test/integration/compute.test.ts
+++ src/test/integration/compute.test.ts
@@ -291,7 +291,7 @@ describe('Compute', () => {
     const resp = await new ComputeInitializeHandler(oceanNode).handle(
       initializeComputeTask
     )
-
+    console.log('response:', resp)
     assert(resp, 'Failed to get response')
     assert(resp.status.httpStatus === 200, 'Failed to get 200 response')
     assert(resp.stream, 'Failed to get stream')
