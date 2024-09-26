fe2362914fe612ee7c71c8cce5b38cc5f4abd65d
paulo-ocean <paulo@oceanprotocol.com>
2024-09-26 14:18:32 +0100

    fix typo on test

--- src/test/unit/oceanP2P.test.ts
+++ src/test/unit/oceanP2P.test.ts
@@ -56,7 +56,7 @@ describe('OceanP2P Test', () => {
     config2.p2pConfig.bootstrapNodes = []
     // enable private IP
     config2.p2pConfig.announcePrivateIp = true
-    config1.p2pConfig.filterAnnouncedAddresses = ['172.15.0.0/24'] // allow nodes to see each other locally for tests
+    config2.p2pConfig.filterAnnouncedAddresses = ['172.15.0.0/24'] // allow nodes to see each other locally for tests
     node2 = new OceanP2P(config2, null)
     await node2.start()
     assert(node2, 'Failed to create P2P Node instance')
