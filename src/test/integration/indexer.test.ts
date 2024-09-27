c2286b471c72b48f38c66deabbc959eec7ef3ed5
AdriGeorge <adrian7ko@gmail.com>
2024-09-27 18:22:22 +0300

    fix: tests

--- src/test/integration/indexer.test.ts
+++ src/test/integration/indexer.test.ts
@@ -256,6 +256,7 @@ describe('Indexer stores a new metadata events and orders.', () => {
   })
 
   it('should store the ddo state in the db with no errors and retrieve it using did', async function () {
+    console.log('resolvedDDO', resolvedDDO)
     const ddoState = await database.ddoState.retrieve(resolvedDDO.id)
     assert(ddoState, 'ddoState not found')
     expect(resolvedDDO.id).to.equal(ddoState.did)
@@ -267,6 +268,7 @@ describe('Indexer stores a new metadata events and orders.', () => {
   it('should find the state of the ddo using query ddo state handler', async function () {
     const queryDdoStateHandler = new QueryDdoStateHandler(oceanNode)
     // query using the did
+    console.log('search for:', resolvedDDO)
     const queryDdoState: QueryCommand = {
       query: {
         q: resolvedDDO.id,
@@ -275,10 +277,12 @@ describe('Indexer stores a new metadata events and orders.', () => {
       command: PROTOCOL_COMMANDS.QUERY
     }
     const response = await queryDdoStateHandler.handle(queryDdoState)
+    console.log('response', response)
     assert(response, 'Failed to get response')
     assert(response.status.httpStatus === 200, 'Failed to get 200 response')
     assert(response.stream, 'Failed to get stream')
     const result = await streamToObject(response.stream as Readable)
+    console.log('result:', result)
     const ddoState = result[0]
     expect(resolvedDDO.id).to.equal(ddoState.did)
     expect(ddoState.valid).to.equal(true)
@@ -456,7 +460,7 @@ describe('Indexer stores a new metadata events and orders.', () => {
       expect(retrievedDDO.stats.orders).to.equal(1)
       initialOrderCount = retrievedDDO.stats.orders
       const resultOrder = await database.order.retrieve(orderTxId)
-      console.log('resultOrder:', resultOrder)
+      console.log('resultOrder', resultOrder)
       expect(resultOrder?.id).to.equal(orderTxId)
       expect(resultOrder?.payer).to.equal(await consumerAccount.getAddress())
       expect(resultOrder?.type).to.equal('startOrder')
@@ -534,7 +538,6 @@ describe('Indexer stores a new metadata events and orders.', () => {
     if (retrievedDDO) {
       expect(retrievedDDO.stats.orders).to.be.greaterThan(initialOrderCount)
       const resultOrder = await database.order.retrieve(reuseOrderTxId)
-      console.log('resultOrder:', resultOrder)
       expect(resultOrder?.id).to.equal(reuseOrderTxId)
       expect(resultOrder?.payer).to.equal(await consumerAccount.getAddress())
       expect(resultOrder?.type).to.equal('reuseOrder')
@@ -652,8 +655,8 @@ describe('OceanIndexer - crawler threads', () => {
       [
         JSON.stringify(supportedNetworks),
         `${homedir}/.ocean/ocean-contracts/artifacts/address.json`,
-        'http://localhost:8108/?apiKey=xyz',
-        DB_TYPES.TYPESENSE
+        'http://localhost:9200',
+        DB_TYPES.ELASTIC_SEARCH
       ]
     )
     envOverrides = await setupEnvironment(null, envOverrides)
