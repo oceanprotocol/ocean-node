c5bcdd88aac59827bdbfb4084de2658b5da0c746
AdriGeorge <adrian7ko@gmail.com>
2024-09-27 18:55:47 +0300

    fix: tests

--- src/test/integration/indexer.test.ts
+++ src/test/integration/indexer.test.ts
@@ -258,6 +258,7 @@ describe('Indexer stores a new metadata events and orders.', () => {
   it('should store the ddo state in the db with no errors and retrieve it using did', async function () {
     console.log('resolvedDDO', resolvedDDO)
     const ddoState = await database.ddoState.retrieve(resolvedDDO.id)
+    console.log('ddoState:', ddoState)
     assert(ddoState, 'ddoState not found')
     expect(resolvedDDO.id).to.equal(ddoState.did)
     expect(ddoState.valid).to.equal(true)
