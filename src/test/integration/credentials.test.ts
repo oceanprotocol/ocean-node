b1df72dd879d4134b8b2f9f9b2b82359f99186e7
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2024-09-27 11:57:07 +0300

    test elastic

--- src/test/integration/credentials.test.ts
+++ src/test/integration/credentials.test.ts
@@ -89,11 +89,11 @@ describe('Should run a complete node flow.', () => {
           JSON.stringify(mockSupportedNetworks),
           JSON.stringify([8996]),
           '0xc594c6e5def4bab63ac29eed19a134c130388f74f019bc74b8f4389df2837a58',
-          'http://localhost:8108/?apiKey=xyz',
+          'http://localhost:9200',
           JSON.stringify(['0xe2DD09d719Da89e5a3D0F2549c7E24566e947260']),
           JSON.stringify(['0xe2DD09d719Da89e5a3D0F2549c7E24566e947260']),
           `${homedir}/.ocean/ocean-contracts/artifacts/address.json`,
-          DB_TYPES.TYPESENSE
+          DB_TYPES.ELASTIC_SEARCH
         ]
       )
     )
