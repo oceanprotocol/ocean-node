5b515af8c1785b255e1cce852ca50287ab900dab
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2024-09-27 11:46:44 +0300

    use typesense

--- src/test/integration/credentials.test.ts
+++ src/test/integration/credentials.test.ts
@@ -93,7 +93,7 @@ describe('Should run a complete node flow.', () => {
           JSON.stringify(['0xe2DD09d719Da89e5a3D0F2549c7E24566e947260']),
           JSON.stringify(['0xe2DD09d719Da89e5a3D0F2549c7E24566e947260']),
           `${homedir}/.ocean/ocean-contracts/artifacts/address.json`,
-          DB_TYPES.ELASTIC_SEARCH
+          DB_TYPES.TYPESENSE
         ]
       )
     )
