18e20679b6d85ce51c2ec839946f2ac1898ca8a7
paulo-ocean <paulo@oceanprotocol.com>
2024-10-14 10:19:01 +0100

    add aux method to help check connections

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -29,3 +29,13 @@ export function hasValidDBConfiguration(configuration: OceanNodeDBConfig): boole
     [DB_TYPES.ELASTIC_SEARCH, DB_TYPES.TYPESENSE].includes(configuration.dbType)
   )
 }
+
+// we can use this to check if DB connection is available
+export async function isReachableConnection(url: string): Promise<boolean> {
+  try {
+    await fetch(url)
+    return true
+  } catch (error) {
+    return false
+  }
+}
