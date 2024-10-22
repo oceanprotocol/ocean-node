1d792797f5481f485d18a379df8a916630c80f27
paulo-ocean <paulo@oceanprotocol.com>
2024-10-22 19:09:28 +0100

    add get running jobs

--- src/test/unit/compute.test.ts
+++ src/test/unit/compute.test.ts
@@ -10,8 +10,7 @@ import {
   DBComputeJob
 } from '../../@types/C2D/C2D.js'
 // import { computeAsset } from '../data/assets'
-import { assert } from 'console'
-import { expect } from 'chai'
+import { assert, expect } from 'chai'
 import {
   convertArrayToString,
   convertStringToArray,
@@ -21,21 +20,21 @@ import {
 describe('Compute Jobs Database', () => {
   let db: C2DDatabase = null
   let jobId: string = null
+
+  const algorithm: ComputeAlgorithm = {
+    documentId: 'did:op:12345',
+    serviceId: '0x1828228'
+  }
+  const dataset: ComputeAsset = {
+    documentId: 'did:op:12345',
+    serviceId: '0x12345abc'
+  }
   before(async () => {
     const config = await getConfiguration(true)
     db = await new C2DDatabase(config.dbConfig, typesenseSchemas.c2dSchemas)
   })
 
   it('should create a new C2D Job', async () => {
-    const algorithm: ComputeAlgorithm = {
-      documentId: 'did:op:12345',
-      serviceId: '0x1828228'
-    }
-    const dataset: ComputeAsset = {
-      documentId: 'did:op:12345',
-      serviceId: '0x12345abc'
-    }
-
     const job: DBComputeJob = {
       owner: '0xe2DD09d719Da89e5a3D0F2549c7E24566e947260',
       jobId: null,
@@ -72,7 +71,6 @@ describe('Compute Jobs Database', () => {
 
   it('should update job', async () => {
     const job = await db.getJob(jobId)
-    console.log('job before update', job)
     // will update some fields
     job.status = C2DStatusNumber.PullImage
     job.isRunning = true
@@ -88,9 +86,48 @@ describe('Compute Jobs Database', () => {
     expect(updatedJob.statusText).to.be.equal(C2DStatusText.PullImage)
   })
 
+  it('should get running jobs', async () => {
+    const job: DBComputeJob = {
+      owner: '0xe2DD09d719Da89e5a3D0F2549c7E24566e947261',
+      jobId: null,
+      dateCreated: null,
+      dateFinished: null,
+      status: C2DStatusNumber.JobStarted,
+      statusText: C2DStatusText.JobStarted,
+      results: null,
+      inputDID: ['did:op:1', 'did:op:2'],
+      expireTimestamp: 1,
+
+      // internal structure
+      clusterHash: 'clusterHash',
+      configlogURL: 'http://localhost:8000',
+      publishlogURL: 'http://localhost:8000',
+      algologURL: 'http://localhost:8000',
+      outputsURL: 'http://localhost:8000',
+      stopRequested: false,
+      algorithm,
+      assets: [dataset],
+      isRunning: false,
+      isStarted: false,
+      containerImage: 'another container image'
+    }
+
+    const jobId = await db.newJob(job)
+    assert(jobId, 'Missing jobId identifier')
+    const existing = await db.getRunningJobs()
+    expect(existing.length === 2, 'No running jobs were found!')
+
+    // Create a filter
+    const withEnv = await db.getRunningJobs(null, 'some environment')
+    expect(withEnv.length === 0, 'No running jobs were found for this environment')
+    // delete it
+    const deleted = await db.deleteJob(jobId)
+    expect(deleted === true, `Job ${jobId} was not deleted!`)
+  })
+
   it('should delete the job by jobId', async () => {
     const deleted = await db.deleteJob(jobId)
-    expect(deleted === true, 'Job was not deleted!')
+    expect(deleted === true, `Job ${jobId} was not deleted!`)
   })
 
   it('should convert array of strings to a string', () => {
