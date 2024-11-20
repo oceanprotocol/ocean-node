8d557b6172effaaf1b886172c78efb98c5dc4518
paulo-ocean <paulo@oceanprotocol.com>
2024-11-20 11:27:05 +0000

    cleanup intergation test, debug

--- src/test/integration/compute.test.ts
+++ src/test/integration/compute.test.ts
@@ -32,7 +32,7 @@ import { OceanNodeConfig } from '../../@types/OceanNode.js'
 import { OceanIndexer } from '../../components/Indexer/index.js'
 import { Readable } from 'stream'
 import { expectedTimeoutFailure, waitToIndex } from './testUtils.js'
-import { getEventFromTx, streamToObject } from '../../utils/util.js'
+import { getEventFromTx, sleep, streamToObject } from '../../utils/util.js'
 import {
   Contract,
   ethers,
@@ -729,11 +729,20 @@ describe('Compute', () => {
     console.log(jobs)
   })
 
-  it('should reject the Free job due to bad container image (directCommand payload)', async () => {
+  // TODO this might change once we check images, architectures, etc
+  it('should delete the Free job due to bad container image (directCommand payload)', async function () {
+    this.timeout(DEFAULT_TEST_TIMEOUT * 2)
     const command: FreeComputeStartCommand = freeComputeStartPayload
     const handler = new FreeComputeStartHandler(oceanNode)
     const response = await handler.handle(command)
-    console.log('response:', response)
+    assert(response.status.httpStatus === 200, 'Failed to get 200 response')
+    assert(response.stream, 'Failed to get stream')
+    const jobs = await streamToObject(response.stream as Readable)
+    console.log('jobs: ', jobs)
+    assert(jobs, 'Failed to get compute job')
+    sleep(DEFAULT_TEST_TIMEOUT / 2)
+    const { c2d } = oceanNode.getDatabase()
+    expect((await c2d.getJob(jobs[0].jobId)) === null, ' job should have been deleted')
   })
 
   it('should checkC2DEnvExists', async () => {
