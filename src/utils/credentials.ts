4d3477cb83f8fea9cf774e759ef87675d78f1ed2
mariacarmina <mariacarmina.cretu@gmail.com>
2025-02-06 14:21:11 +0200

    fix import.

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,5 +1,4 @@
 import { Credential, Credentials } from '../@types/DDO/Credentials'
-import { PROVIDER_LOGGER } from './logging/common'
 
 export function findCredential(
   credentials: Credential[],
@@ -26,15 +25,10 @@ export function findCredential(
  * @param consumerAddress consumer address
  */
 export function checkCredentials(credentials: Credentials, consumerAddress: string) {
-  PROVIDER_LOGGER.logMessage(`check credentials started!`)
   const consumerCredentials = {
     type: 'address',
     values: [String(consumerAddress)?.toLowerCase()]
   }
-  PROVIDER_LOGGER.logMessage(
-    `consumerCredentials: ${JSON.stringify(consumerCredentials)}`
-  )
-  PROVIDER_LOGGER.logMessage(`credentials: ${JSON.stringify(credentials)}`)
   // check deny access
   if (Array.isArray(credentials?.deny) && credentials.deny.length > 0) {
     const accessDeny = findCredential(credentials.deny, consumerCredentials)
