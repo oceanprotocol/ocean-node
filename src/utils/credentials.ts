6db29242f69b527fb11174207c75a0b28f3162aa
paulo-ocean <paulo@oceanprotocol.com>
2025-02-07 10:05:56 +0000

    add utility function for access list credentials

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,4 +1,8 @@
 import { Credential, Credentials } from '../@types/DDO/Credentials'
+import AccessList from '@oceanprotocol/contracts/artifacts/contracts/accesslists/AccessList.sol/AccessList.json' assert { type: 'json' }
+import { AccessListContract } from '../@types/OceanNode.js'
+import { ethers, Signer } from 'ethers'
+import { CORE_LOGGER } from './logging/common'
 
 export function findCredential(
   credentials: Credential[],
@@ -24,7 +28,10 @@ export function findCredential(
  * @param credentials credentials
  * @param consumerAddress consumer address
  */
-export function checkCredentials(credentials: Credentials, consumerAddress: string) {
+export function checkCredentials(
+  credentials: Credentials,
+  consumerAddress: string
+): boolean {
   const consumerCredentials = {
     type: 'address',
     values: [String(consumerAddress)?.toLowerCase()]
@@ -42,6 +49,50 @@ export function checkCredentials(credentials: Credentials, consumerAddress: stri
     if (!accessAllow) {
       return false
     }
+  }
+  return true
+}
+
+// utility function that can be used on multiple access lists
+/**
+ * @param accessList the access list contract address
+ * @param chainId the chain id to check
+ * @param addressToCheck the account address to check on the access list
+ * @param signer signer for the contract part
+ * @returns true if the account has balanceOf > 0 OR if the accessList is empty OR does not contain info for this chain, false otherwise
+ */
+export async function checkCredentialOnAccessList(
+  accessList: AccessListContract,
+  chainId: string,
+  addressToCheck: string,
+  signer: Signer
+): Promise<boolean> {
+  if (!accessList) {
+    return true
+  }
+  const chainsListed = Object.keys(accessList)
+  // check the access lists for this chain
+  if (chainsListed.length > 0 && chainsListed.includes(chainId)) {
+    let isAuthorized = false
+    for (const accessListAddress of accessList[chainId]) {
+      const accessListContract = new ethers.Contract(
+        accessListAddress,
+        AccessList.abi,
+        signer
+      )
+      // if has at least 1 token than is is authorized
+      const balance = await accessListContract.balanceOf(addressToCheck)
+      if (Number(balance) > 0) {
+        isAuthorized = true
+        break
+      }
+    }
+    if (!isAuthorized) {
+      CORE_LOGGER.error(
+        `Account ${addressToCheck} is NOT part of the given access list group.`
+      )
+    }
+    return isAuthorized
   }
   return true
 }
