eb3a29e55b37ac4b1be72b60deb59689413f1d61
Jamie Hewitt <jamie.hewitt15@gmail.com>
2025-02-13 16:37:12 +0300

    Test if the dashboard test fails when changes haven't been included in the build files

--- dashboard/src/pages/index.tsx
+++ dashboard/src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Ocean nodes</title>
+        <title>Ocean nodes - TEST</title>
         <meta name="description" content="Ocean nodes dashboard" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
