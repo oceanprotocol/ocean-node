3c518d6c37b1a76ad8fba32a14e24b4241a8ca78
Jamie Hewitt <jamie.hewitt15@gmail.com>
2025-03-07 16:16:01 +0300

    Update unit test

--- src/test/unit/indexer/indexer.test.ts
+++ src/test/unit/indexer/indexer.test.ts
@@ -4,13 +4,11 @@ import { OceanIndexer } from '../../../components/Indexer/index.js'
 import { getConfiguration } from '../../../utils/index.js'
 import { Database } from '../../../components/database/index.js'
 import { OceanNodeConfig } from '../../../@types/OceanNode.js'
-import { RPCS } from '../../../@types/blockchain.js'
 import {
   hasValidDBConfiguration,
   isReachableConnection
 } from '../../../utils/database.js'
-import sinon from 'sinon'
-import * as dbUtils from '../../../utils/database.js'
+import { isReindexingNeeded } from '../../../components/Indexer/version.js'
 
 describe('OceanIndexer', () => {
   let oceanIndexer: OceanIndexer
@@ -41,97 +39,5 @@ describe('OceanIndexer', () => {
 
     // there are no worker threads available
     expect(oceanIndexer.stopAllThreads()).to.be.equal(false)
-  })
-
-  describe('checkAndTriggerReindexing', () => {
-    let indexer: OceanIndexer
-    let mockDb: Database
-    let resetCrawlingSpy: sinon.SinonSpy
-    let isReachableStub: sinon.SinonStub
-
-    beforeEach(() => {
-      // Create mock database with indexer property
-      mockDb = {
-        getConfig: () => ({ url: 'http://localhost:9200', dbType: 'elasticsearch' }),
-        indexer: {
-          getNodeVersion: sinon.stub().resolves(null),
-          setNodeVersion: sinon.stub().resolves()
-        }
-      } as unknown as Database
-
-      const mockNetworks = {
-        '8996': {
-          chainId: 8996,
-          network: '8996',
-          rpc: 'http://localhost:8545'
-        }
-      } as RPCS
-
-      // Mock isReachableConnection to return true
-      isReachableStub = sinon.stub(dbUtils, 'isReachableConnection').resolves(true)
-
-      // Create indexer with mock database
-      indexer = new OceanIndexer(mockDb, mockNetworks)
-
-      // Spy on resetCrawling method
-      resetCrawlingSpy = sinon.spy(indexer, 'resetCrawling')
-
-      // Mock getCurrentVersion method
-      sinon.stub(indexer, 'getCurrentVersion' as any).returns('0.2.2')
-    })
-
-    afterEach(() => {
-      sinon.restore()
-    })
-
-    it('should have null version initially', async () => {
-      const version = await mockDb.indexer.getNodeVersion()
-      assert(version === null, 'Initial version should be null')
-    })
-
-    it('should trigger reindexing and set version when version is null', async () => {
-      await indexer.checkAndTriggerReindexing()
-
-      assert(resetCrawlingSpy.calledOnce, 'resetCrawling should be called once')
-      assert(
-        (mockDb.indexer.setNodeVersion as sinon.SinonStub).calledWith('0.2.2'),
-        'setNodeVersion should be called with current version'
-      )
-    })
-
-    it('should trigger reindexing when database version is old', async () => {
-      // Set an old version in DB
-      ;(mockDb.indexer.getNodeVersion as sinon.SinonStub).resolves('0.2.0')
-
-      await indexer.checkAndTriggerReindexing()
-
-      assert(resetCrawlingSpy.calledOnce, 'resetCrawling should be called once')
-      assert(
-        (mockDb.indexer.setNodeVersion as sinon.SinonStub).calledWith('0.2.2'),
-        'setNodeVersion should be called with new version'
-      )
-    })
-
-    it('should not trigger reindexing when database version is current', async () => {
-      // Set current version in DB
-      ;(mockDb.indexer.getNodeVersion as sinon.SinonStub).resolves('0.2.2')
-
-      await indexer.checkAndTriggerReindexing()
-
-      assert(resetCrawlingSpy.notCalled, 'resetCrawling should not be called')
-    })
-
-    it('should not proceed if database is not reachable', async () => {
-      // Make database unreachable
-      isReachableStub.resolves(false)
-
-      await indexer.checkAndTriggerReindexing()
-
-      assert(resetCrawlingSpy.notCalled, 'resetCrawling should not be called')
-      assert(
-        (mockDb.indexer.setNodeVersion as sinon.SinonStub).notCalled,
-        'setNodeVersion should not be called'
-      )
-    })
   })
 })
