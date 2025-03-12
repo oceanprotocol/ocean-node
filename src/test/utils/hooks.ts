03eb47990ce3b68ce480f9730853d29ae2201325
paulo-ocean <paulo@oceanprotocol.com>
2025-03-12 17:53:16 +0000

    restor hooks

--- src/test/utils/hooks.ts
+++ src/test/utils/hooks.ts
@@ -2,14 +2,8 @@
 // beforeAll() and afterAll() are called before starting the tests
 // and after finishing them respectively.
 
-import {
-  DEVELOPMENT_CHAIN_ID,
-  getOceanArtifactsAdresses,
-  getOceanArtifactsAdressesByChainId
-} from '../../utils/address.js'
-import { DB_TYPES, ENVIRONMENT_VARIABLES, EnvVariable } from '../../utils/constants.js'
+import { DB_TYPES, ENVIRONMENT_VARIABLES } from '../../utils/constants.js'
 import { CONFIG_LOGGER } from '../../utils/logging/common.js'
-import { deployAccessListContract, getContract } from './contracts.js'
 import {
   setupEnvironment,
   OverrideEnvConfig,
@@ -21,12 +15,6 @@ import {
   SELECTED_RUN_DATABASE
 } from './utils.js'
 
-import AccessListFactory from '@oceanprotocol/contracts/artifacts/contracts/accesslists/AccessListFactory.sol/AccessListFactory.json' assert { type: 'json' }
-import AccessList from '@oceanprotocol/contracts/artifacts/contracts/accesslists/AccessList.sol/AccessList.json' assert { type: 'json' }
-import { ethers, JsonRpcProvider, Signer } from 'ethers'
-import { AccessListContract } from '../../@types/OceanNode.js'
-import { printCurrentConfig } from '../../utils/index.js'
-
 // current process.env environment
 // save any existing configuration before starting the tests
 const initialConfiguration: Map<string, OverrideEnvConfig> = getExistingEnvironment()
@@ -37,104 +25,46 @@ let initialSetupDone = false
 // this function returns the new configuration together with any variables that were overrided
 // at the end call tearDownEnvironment() with the previously saved configuration (on after() function)
 // example of override configuration:
-function getEnvOverrides(
-  envVars: EnvVariable[] = [],
-  envValues: any[] = []
-): OverrideEnvConfig[] {
-  const initialEnvVariables = [
-    ENVIRONMENT_VARIABLES.IPFS_GATEWAY,
-    ENVIRONMENT_VARIABLES.ARWEAVE_GATEWAY,
-    ENVIRONMENT_VARIABLES.RPCS,
-    ENVIRONMENT_VARIABLES.PRIVATE_KEY,
-    ENVIRONMENT_VARIABLES.DB_TYPE,
-    ENVIRONMENT_VARIABLES.DB_URL,
-    ...envVars
-  ]
-
-  const initialEnvValues = [
-    'http://172.15.0.16:8080/',
-    'https://arweave.net/',
-    '{ "1": {"rpc": "https://rpc.eth.gateway.fm", "chainId": 1, "network": "mainet", "chunkSize": 100}, "137": {"rpc": "https://polygon.meowrpc.com", "chainId": 137, "network": "polygon", "chunkSize": 100 }}',
-    '0xc594c6e5def4bab63ac29eed19a134c130388f74f019bc74b8f4389df2837a58',
-    SELECTED_RUN_DATABASE,
-    SELECTED_RUN_DATABASE === DB_TYPES.ELASTIC_SEARCH
-      ? 'http://localhost:9200'
-      : 'http://localhost:8108/?apiKey=xyz',
-    ...envValues
-  ]
-  console.log('initialEnvVariables:', initialEnvVariables)
-  console.log('initialEnvValues:', initialEnvValues)
+function getEnvOverrides(): OverrideEnvConfig[] {
   // we can also use the function buildEnvOverrideConfig([variables],[values]) to build that array
-  return buildEnvOverrideConfig(initialEnvVariables, initialEnvValues)
-}
-
-async function deployAccessLists(
-  envVariable: string
-): Promise<AccessListContract | null> {
-  const provider = new JsonRpcProvider('http://127.0.0.1:8545')
-  let networkArtifacts = getOceanArtifactsAdressesByChainId(DEVELOPMENT_CHAIN_ID)
-  if (!networkArtifacts) {
-    networkArtifacts = getOceanArtifactsAdresses().development
-  }
-
-  const owner = new ethers.Wallet(ENVIRONMENT_VARIABLES.PRIVATE_KEY.value) as Signer
-
-  const wallets = [
-    (await provider.getSigner(0)) as Signer,
-    (await provider.getSigner(1)) as Signer,
-    (await provider.getSigner(2)) as Signer,
-    (await provider.getSigner(3)) as Signer
-  ]
-  const txAddress = await deployAccessListContract(
-    owner, // owner is first account
-    networkArtifacts.AccessListFactory,
-    AccessListFactory.abi,
-    'AllowList',
-    'ALLOW',
-    false,
-    await owner.getAddress(),
+  return buildEnvOverrideConfig(
     [
-      await wallets[0].getAddress(),
-      await wallets[1].getAddress(),
-      await wallets[2].getAddress(),
-      await wallets[3].getAddress()
+      ENVIRONMENT_VARIABLES.IPFS_GATEWAY,
+      ENVIRONMENT_VARIABLES.ARWEAVE_GATEWAY,
+      ENVIRONMENT_VARIABLES.RPCS,
+      ENVIRONMENT_VARIABLES.PRIVATE_KEY,
+      ENVIRONMENT_VARIABLES.DB_TYPE,
+      ENVIRONMENT_VARIABLES.DB_URL
     ],
-    ['https://oceanprotocol.com/nft/']
+    [
+      'http://172.15.0.16:8080/',
+      'https://arweave.net/',
+      '{ "8996": {"rpc": "http://127.0.0.1:8545", "chainId": 8996, "network": "development", "chunkSize": 100}, "137": {"rpc": "https://polygon.meowrpc.com", "chainId": 137, "network": "polygon", "chunkSize": 100 }}',
+      '0xc594c6e5def4bab63ac29eed19a134c130388f74f019bc74b8f4389df2837a58',
+      SELECTED_RUN_DATABASE,
+      SELECTED_RUN_DATABASE === DB_TYPES.ELASTIC_SEARCH
+        ? 'http://localhost:9200'
+        : 'http://localhost:8108/?apiKey=xyz'
+    ]
   )
-  console.log('txAddress: ', txAddress)
-
-  const contractAcessList = getContract(txAddress, AccessList.abi, owner)
-  console.log('contractAcessList:', contractAcessList)
-  return contractAcessList ? { DEVELOPMENT_CHAIN_ID: [txAddress] } : null
 }
 
 export const mochaHooks = {
   beforeAll() {
-    deployAccessLists(ENVIRONMENT_VARIABLES.ALLOWED_VALIDATORS_LIST.name).then(
-      (accessListAddress) => {
-        console.log('accessListAddress: ', accessListAddress)
-        if (accessListAddress) {
-          const envVars: EnvVariable[] = [ENVIRONMENT_VARIABLES.ALLOWED_VALIDATORS_LIST]
-          const envValues: any[] = [JSON.stringify(accessListAddress)]
-          envOverrides = getEnvOverrides(envVars, envValues)
-        } else {
-          // get stuff we want to override
-          envOverrides = getEnvOverrides()
-        }
-        // if it exists will use it, otherwise nothing happens
-        // in any case it WILL NOT override the existing configuration
-        // it returns the original object with the original value preserved to be restored later
-        setupEnvironment(TEST_ENV_CONFIG_FILE, envOverrides).then((overrides) => {
-          envOverrides = overrides
-        })
-        initialSetupDone = true
-        CONFIG_LOGGER.debug(`(Hook) Initial test setup: ${JSON.stringify(envOverrides)} `)
-        printCurrentConfig()
+    // get stuff we want to override
+    envOverrides = getEnvOverrides()
 
-        // just in case the configuration value fails
-        this.timeout(DEFAULT_TEST_TIMEOUT)
-      }
-    )
+    // if it exists will use it, otherwise nothing happens
+    // in any case it WILL NOT override the existing configuration
+    // it returns the original object with the original value preserved to be restored later
+    setupEnvironment(TEST_ENV_CONFIG_FILE, envOverrides).then((overrides) => {
+      envOverrides = overrides
+    })
+    initialSetupDone = true
+    CONFIG_LOGGER.debug(`(Hook) Initial test setup: ${JSON.stringify(envOverrides)} `)
+
+    // just in case the configuration value fails
+    this.timeout(DEFAULT_TEST_TIMEOUT)
   },
 
   afterAll() {
