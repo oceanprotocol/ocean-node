181cadc259eb01939bd58e92544dedd57de2f9ba
paulo-ocean <paulo@oceanprotocol.com>
2025-03-13 14:44:21 +0000

    add 2 ore tests

--- src/test/integration/accessLists.test.ts
+++ src/test/integration/accessLists.test.ts
@@ -12,11 +12,12 @@ import { RPCS, SupportedNetwork } from '../../@types/blockchain.js'
 import { DEVELOPMENT_CHAIN_ID } from '../../utils/address.js'
 import { ENVIRONMENT_VARIABLES } from '../../utils/constants.js'
 import { deployAndGetAccessListConfig } from '../utils/contracts.js'
-import { OceanNodeConfig } from '../../@types/OceanNode.js'
+import { AccessListContract, OceanNodeConfig } from '../../@types/OceanNode.js'
 import { homedir } from 'os'
 import { getConfiguration } from '../../utils/config.js'
 import { EXISTING_ACCESSLISTS } from '../utils/hooks.js'
 import { assert, expect } from 'chai'
+import { findAccessListCredentials } from '../../utils/credentials.js'
 
 describe('Should deploy some accessLists before all other tests.', () => {
   let config: OceanNodeConfig
@@ -34,6 +35,8 @@ describe('Should deploy some accessLists before all other tests.', () => {
   let owner: Signer
 
   let wallets: Signer[] = []
+
+  let allAccessListsDefinitions: AccessListContract[] = []
 
   before(async () => {
     provider = new JsonRpcProvider('http://127.0.0.1:8545')
@@ -58,22 +61,44 @@ describe('Should deploy some accessLists before all other tests.', () => {
     owner = blockchain.getSigner()
 
     // ENVIRONMENT_VARIABLES.AUTHORIZED_PUBLISHERS_LIST
-    const accessList = await deployAndGetAccessListConfig(owner, provider, wallets)
+    const accessListPublishers = await deployAndGetAccessListConfig(
+      owner,
+      provider,
+      wallets
+    )
     EXISTING_ACCESSLISTS.set(
       ENVIRONMENT_VARIABLES.AUTHORIZED_PUBLISHERS_LIST.name,
-      accessList
+      accessListPublishers
+    )
+    const accessListValidators = await deployAndGetAccessListConfig(
+      owner,
+      provider,
+      wallets
     )
     // ENVIRONMENT_VARIABLES.ALLOWED_VALIDATORS_LIST
     EXISTING_ACCESSLISTS.set(
       ENVIRONMENT_VARIABLES.ALLOWED_VALIDATORS_LIST.name,
-      accessList
+      accessListValidators
     )
 
+    const accessListDecrypters = await deployAndGetAccessListConfig(
+      owner,
+      provider,
+      wallets
+    )
     // ENVIRONMENT_VARIABLES.AUTHORIZED_DECRYPTERS_LIST
     EXISTING_ACCESSLISTS.set(
       ENVIRONMENT_VARIABLES.AUTHORIZED_DECRYPTERS_LIST.name,
-      accessList
+      accessListDecrypters
     )
+
+    // put them all here
+    allAccessListsDefinitions = [
+      accessListPublishers,
+      accessListValidators,
+      accessListDecrypters
+    ]
+
     // override and save configuration (always before calling getConfig())
     previousConfiguration = await setupEnvironment(
       TEST_ENV_CONFIG_FILE,
@@ -147,6 +172,32 @@ describe('Should deploy some accessLists before all other tests.', () => {
       `${ENVIRONMENT_VARIABLES.AUTHORIZED_DECRYPTERS_LIST.name}  accessList is not defined`
     )
     console.log(config.authorizedPublishersList)
+  })
+
+  it('should check if wallets are on accessList', async function () {
+    for (let z = 0; z < allAccessListsDefinitions.length; z++) {
+      for (let i = 0; i < wallets.length; i++) {
+        const account = await wallets[i].getAddress()
+        const accessListAddress = allAccessListsDefinitions[z][DEVELOPMENT_CHAIN_ID][0] // we have only 1 accesslist per config
+        expect(
+          (await findAccessListCredentials(owner, account, accessListAddress)) === true,
+          `Address ${account} has no balance on Access List ${accessListAddress}, so its not Authorized`
+        )
+      }
+    }
+  })
+
+  it('should check that wallets are NOT on accessList', async function () {
+    for (let z = 0; z < allAccessListsDefinitions.length; z++) {
+      for (let i = wallets.length; i < 4; i++) {
+        const account = await (await provider.getSigner(i)).getAddress()
+        const accessListAddress = allAccessListsDefinitions[z][DEVELOPMENT_CHAIN_ID][0] // we have only 1 accesslist per config
+        expect(
+          (await findAccessListCredentials(owner, account, accessListAddress)) === true,
+          `Address ${account} has no balance on Access List ${accessListAddress}, so its not Authorized`
+        )
+      }
+    }
   })
 
   after(async () => {
