eb9d9e1cfb747284e5b5d866026cac87c75bce35
Bogdan Fazakas <bogdan.fazakas@gmail.com>
2025-07-17 16:44:36 +0300

    Added more logs

--- src/components/database/ElasticsearchConfigHelper.ts
+++ src/components/database/ElasticsearchConfigHelper.ts
@@ -1,5 +1,7 @@
 import { Client } from '@elastic/elasticsearch'
 import { OceanNodeDBConfig } from '../../@types'
+import { DATABASE_LOGGER } from '../../utils/logging/common.js'
+import { GENERIC_EMOJIS, LOG_LEVELS_STR } from '../../utils/logging/Logger.js'
 
 export interface ElasticsearchRetryConfig {
   requestTimeout?: number
@@ -30,6 +32,8 @@ class ElasticsearchClientSingleton {
   private static instance: any = null
   private client: Client | null = null
   private config: OceanNodeDBConfig | null = null
+  private connectionAttempts: number = 0
+  private lastConnectionTime: number = 0
 
   private constructor() {}
 
@@ -48,28 +52,75 @@ class ElasticsearchClientSingleton {
       return this.client
     }
 
+    return this.createNewConnection(config, customConfig)
+  }
+
+  private createNewConnection(
+    config: OceanNodeDBConfig,
+    customConfig: Partial<ElasticsearchRetryConfig> = {}
+  ): Client {
+    this.connectionAttempts++
+    this.lastConnectionTime = Date.now()
+
     const finalConfig = {
       ...DEFAULT_ELASTICSEARCH_CONFIG,
       ...customConfig
     }
 
-    this.client = new Client({
-      node: config.url,
-      auth:
-        config.username && config.password
-          ? { username: config.username, password: config.password }
-          : undefined,
-      requestTimeout: finalConfig.requestTimeout,
-      pingTimeout: finalConfig.pingTimeout,
-      resurrectStrategy: finalConfig.resurrectStrategy,
-      maxRetries: finalConfig.maxRetries,
-      sniffOnStart: finalConfig.sniffOnStart,
-      sniffInterval: finalConfig.sniffInterval,
-      sniffOnConnectionFault: finalConfig.sniffOnConnectionFault
-    })
+    try {
+      this.client = new Client({
+        node: config.url,
+        auth:
+          config.username && config.password
+            ? { username: config.username, password: config.password }
+            : undefined,
+        requestTimeout: finalConfig.requestTimeout,
+        pingTimeout: finalConfig.pingTimeout,
+        resurrectStrategy: finalConfig.resurrectStrategy,
+        maxRetries: finalConfig.maxRetries,
+        sniffOnStart: finalConfig.sniffOnStart,
+        sniffInterval: finalConfig.sniffInterval,
+        sniffOnConnectionFault: finalConfig.sniffOnConnectionFault
+      })
 
-    this.config = { ...config }
-    return this.client
+      this.config = { ...config }
+
+      DATABASE_LOGGER.logMessageWithEmoji(
+        `Elasticsearch connection established successfully to ${this.maskUrl(
+          config.url
+        )} (attempt ${this.connectionAttempts}/${
+          finalConfig.maxRetries
+        }) last successful connection ${this.lastConnectionTime}`,
+        true,
+        GENERIC_EMOJIS.EMOJI_CHECK_MARK,
+        LOG_LEVELS_STR.LEVEL_INFO
+      )
+
+      return this.client
+    } catch (error) {
+      DATABASE_LOGGER.logMessageWithEmoji(
+        `Failed to connect to Elasticsearch at ${this.maskUrl(config.url)} (attempt ${
+          this.connectionAttempts
+        }/${finalConfig.maxRetries}) last successful connection ${
+          this.lastConnectionTime
+        }: ${error.message}`,
+        true,
+        GENERIC_EMOJIS.EMOJI_CROSS_MARK,
+        LOG_LEVELS_STR.LEVEL_ERROR
+      )
+      throw error
+    }
+  }
+
+  private maskUrl(url: string): string {
+    try {
+      const urlObj = new URL(url)
+      return `${urlObj.protocol}//${urlObj.hostname}:${
+        urlObj.port || (urlObj.protocol === 'https:' ? '443' : '80')
+      }`
+    } catch (error) {
+      return url.replace(/\/\/[^@]+@/, '//***:***@')
+    }
   }
 }
 
