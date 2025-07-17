6c9b6385c8de121c7b2a57ab7bdf3ce0272a3986
mariacarmina <mariacarmina.cretu@gmail.com>
2025-07-17 18:07:38 +0300

    Created new util function for checking global connections. Implemented the rate limiters checks for P2P protocol as well.

--- src/components/httpRoutes/requestValidator.ts
+++ src/components/httpRoutes/requestValidator.ts
@@ -2,24 +2,15 @@ import { Request, Response } from 'express'
 import { getConfiguration } from '../../utils/config.js'
 import { HTTP_LOGGER } from '../../utils/logging/common.js'
 import { OceanNodeConfig } from '../../@types/OceanNode.js'
-import { RequestLimiter } from '../../OceanNode.js'
 import {
-  CONNECTIONS_RATE_INTERVAL,
-  DEFAULT_MAX_CONNECTIONS_PER_MINUTE
-} from '../../utils/constants.js'
-
-// TODO we should group common stuff,
-// we have multiple similar validation interfaces
-export interface CommonValidation {
-  valid: boolean
-  error?: string
-}
-
-// hold data about last request made
-const connectionsData = new Map<string, RequestLimiter>()
+  checkGlobalConnectionsRateLimit,
+  checkRequestsRateLimit,
+  CommonValidation
+} from '../../utils/validators.js'
 
 // Middleware to validate IP and apply rate limiting
 export const requestValidator = async function (req: Request, res: Response, next: any) {
+  const now = Date.now()
   const requestIP = (req.headers['x-forwarded-for'] ||
     req.socket.remoteAddress ||
     '') as string
@@ -32,7 +23,7 @@ export const requestValidator = async function (req: Request, res: Response, nex
     return res.status(403).send(ipValidation.error)
   }
 
-  const rateLimitCheck = checkRequestsRateLimit(requestIP, configuration)
+  const rateLimitCheck = checkRequestsRateLimit(requestIP, configuration, now)
   if (!rateLimitCheck.valid) {
     HTTP_LOGGER.logMessage(
       `Exceeded limit of requests per minute ${configuration.rateLimit}: ${rateLimitCheck.error}`
@@ -40,71 +31,13 @@ export const requestValidator = async function (req: Request, res: Response, nex
     return res.status(429).send(rateLimitCheck.error)
   }
 
-  const requestRateValidation = checkConnectionsRateLimit(
-    configuration,
-    connectionsData.get(requestIP)
-  )
-  if (!requestRateValidation.valid) {
-    HTTP_LOGGER.logMessage(
-      `Exceeded limit of connections per minute ${configuration.maxConnections}: ${requestRateValidation.error}`
-    )
-    res.status(403).send(requestRateValidation.error)
+  const connectionsRateValidation = checkGlobalConnectionsRateLimit(configuration, now)
+  if (!connectionsRateValidation.valid) {
+    res.status(403).send(connectionsRateValidation.error)
     return
   }
 
   next()
-}
-
-function checkRequestsRateLimit(
-  requestIP: string,
-  configuration: OceanNodeConfig
-): CommonValidation {
-  const requestTime = Date.now()
-  const limit = configuration.rateLimit
-  let clientData = connectionsData.get(requestIP)
-
-  if (!clientData || clientData === undefined) {
-    clientData = {
-      lastRequestTime: requestTime,
-      requester: requestIP,
-      numRequests: 1
-    }
-    connectionsData.set(requestIP, clientData)
-    return { valid: true, error: '' }
-  }
-
-  const timeSinceLastRequest = requestTime - clientData.lastRequestTime
-  const windowExpired = timeSinceLastRequest > CONNECTIONS_RATE_INTERVAL
-
-  if (clientData.numRequests >= limit && !windowExpired) {
-    const waitTime = Math.ceil((CONNECTIONS_RATE_INTERVAL - timeSinceLastRequest) / 1000)
-    return {
-      valid: false,
-      error: `Rate limit exceeded. Try again in ${waitTime} seconds.`
-    }
-  }
-
-  if (windowExpired) {
-    clientData.numRequests = 1
-    clientData.lastRequestTime = requestTime
-    return { valid: true, error: '' }
-  }
-
-  clientData.numRequests += 1
-  return { valid: true, error: '' }
-}
-
-export function checkConnectionsRateLimit(
-  configuration: OceanNodeConfig,
-  connectionsData: RequestLimiter
-): CommonValidation {
-  const connectionLimits =
-    configuration.maxConnections || DEFAULT_MAX_CONNECTIONS_PER_MINUTE
-  const ok = connectionsData.numRequests <= connectionLimits
-  return {
-    valid: ok,
-    error: ok ? '' : 'Unauthorized request. Rate limit exceeded!'
-  }
 }
 
 function checkIP(
