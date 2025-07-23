8b03ad0a63bc5fb52274367d532840e683716c2b
mariacarmina <mariacarmina.cretu@gmail.com>
2025-07-23 11:23:38 +0300

    Fix setup environment in indexer unit tests.

--- src/test/unit/indexer/indexer.test.ts
+++ src/test/unit/indexer/indexer.test.ts
@@ -1,19 +1,33 @@
 import { assert, expect } from 'chai'
 import { describe, it } from 'mocha'
 import { OceanIndexer } from '../../../components/Indexer/index.js'
-import { getConfiguration } from '../../../utils/index.js'
+import { ENVIRONMENT_VARIABLES, getConfiguration } from '../../../utils/index.js'
 import { Database } from '../../../components/database/index.js'
 import { OceanNodeConfig } from '../../../@types/OceanNode.js'
 import {
   hasValidDBConfiguration,
   isReachableConnection
 } from '../../../utils/database.js'
+import {
+  buildEnvOverrideConfig,
+  OverrideEnvConfig,
+  setupEnvironment,
+  tearDownEnvironment
+} from '../../utils/utils.js'
 
 describe('OceanIndexer', () => {
+  let envOverrides: OverrideEnvConfig[]
   let oceanIndexer: OceanIndexer
   let mockDatabase: Database
   let config: OceanNodeConfig
   before(async () => {
+    envOverrides = buildEnvOverrideConfig(
+      [ENVIRONMENT_VARIABLES.RPCS],
+      [
+        '{ "8996":{ "rpc":"http://127.0.0.1:8545", "fallbackRPCs": ["http://127.0.0.3:8545","http://127.0.0.1:8545"], "chainId": 8996, "network": "development", "chunkSize": 100 }}'
+      ]
+    )
+    envOverrides = await setupEnvironment(null, envOverrides)
     config = await getConfiguration(true)
     mockDatabase = await new Database(config.dbConfig)
   })
@@ -38,5 +52,8 @@ describe('OceanIndexer', () => {
 
     // there are no worker threads available
     expect(oceanIndexer.stopAllThreads()).to.be.equal(false)
+  })
+  after(async () => {
+    await tearDownEnvironment(envOverrides)
   })
 })
