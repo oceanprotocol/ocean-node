8bc8d2c0b1e76a718920f40dd0d9126d8a8ff79e
giurgiur99 <giurgiur99@gmail.com>
2025-09-30 13:18:42 +0300

    lint

--- src/@types/humanhash.d.ts
+++ src/@types/humanhash.d.ts
@@ -1,9 +1,13 @@
 declare module 'humanhash' {
-    class HumanHasher {
-        constructor(wordlist?: string[])
-        humanize(hexdigest: string, words?: number, separator?: string): string
-        uuid(words?: number, separator?: string, version?: number): { humanhash: string; uuid: string }
-    }
+  class HumanHasher {
+    constructor(wordlist?: string[])
+    humanize(hexdigest: string, words?: number, separator?: string): string
+    uuid(
+      words?: number,
+      separator?: string,
+      version?: number
+    ): { humanhash: string; uuid: string }
+  }
 
-    export = HumanHasher
+  export = HumanHasher
 }
