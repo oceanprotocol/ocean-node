caf98bd871d5847052300e81362b390fb578d8cf
Giurgiu Razvan <giurgiur99@gmail.com>
2025-10-15 14:02:33 +0300

    fix fees filter (#1073)
    * fix fees filter
    
    * lint fix

--- src/components/core/utils/feesHandler.ts
+++ src/components/core/utils/feesHandler.ts
@@ -469,9 +469,9 @@ export async function getProviderKey(): Promise<string> {
  * @returns the token address
  */
 export async function getProviderFeeToken(chainId: number): Promise<string> {
-  const result = (await getConfiguration()).feeStrategy.feeTokens.filter(
-    (token: FeeTokens) => Number(token.chain) === chainId
-  )
+  const config = await getConfiguration()
+  const feeTokens = config?.feeStrategy?.feeTokens || []
+  const result = feeTokens.filter((token: FeeTokens) => Number(token.chain) === chainId)
   if (result.length === 0 && chainId === 8996) {
     const localOceanToken = getOceanArtifactsAdresses().development.Ocean
     return localOceanToken || ethers.ZeroAddress
