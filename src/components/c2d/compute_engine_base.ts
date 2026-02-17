74a04ad63bd26ad1197c83a19a0f2bab97f05410
denisiuriet <iuriet.denis@gmail.com>
2026-02-17 12:29:56 +0200

    add more logs

--- src/components/c2d/compute_engine_base.ts
+++ src/components/c2d/compute_engine_base.ts
@@ -538,8 +538,14 @@ export abstract class C2DEngine {
     if (!prices) return null
     let cost: number = 0
     for (const request of resourcesRequest) {
+      CORE_LOGGER.logMessage(`Resource ${request.id} with amount ${request.amount}`)
+
       const resourcePrice = this.getResourcePrice(prices, request.id)
-      cost += resourcePrice * request.amount * Math.ceil(maxJobDuration / 60)
+      CORE_LOGGER.logMessage(`Resource price ${resourcePrice}`)
+      const resourceCost = resourcePrice * request.amount * Math.ceil(maxJobDuration / 60)
+      CORE_LOGGER.logMessage(`Resource cost ${resourceCost}`)
+
+      cost += resourceCost
     }
     return cost
   }
